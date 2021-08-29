544dd2827e29fc48631324d04cfedbabd9f4170f
Ole Jørgen Rishoff <olejri@gmail.com>
2021-08-29 22:41:13 +0200

    CSV format

--- src/main/kotlin/no/nav/k9/tjenester/innsikt/Innsikt.kt
+++ src/main/kotlin/no/nav/k9/tjenester/innsikt/Innsikt.kt
@@ -90,7 +90,7 @@ fun Route.innsiktGrensesnitt() {
                         for (behandslingstype in behandlingstyper.keys) {
                             div {
                                 classes = setOf("input-group-text display-4")
-                                + "Dato: ${dato} - Type ${behandslingstype.navn} - AntallPunsjOppgaver: ${behandlingstyper.values.size}"
+                                + "${dato},${behandslingstype.navn},${behandlingstyper.values.size}"
                             }
                         }
                     }
