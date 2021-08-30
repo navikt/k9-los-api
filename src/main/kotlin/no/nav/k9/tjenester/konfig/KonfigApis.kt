002062c8b6336af31cf47d84f63ca965a7d8f0ed
Erik M.Forsman <fraadsbrandth@users.noreply.github.com>
2021-08-30 14:21:04 +0200

    Gamle URL'er mot k9-los-oidc-auth-proxy

--- src/main/kotlin/no/nav/k9/tjenester/konfig/KonfigApis.kt
+++ src/main/kotlin/no/nav/k9/tjenester/konfig/KonfigApis.kt
@@ -13,8 +13,8 @@ fun Route.KonfigApis() {
     val configuration by inject<Configuration>()
     val k9sakUrlDev = "https://app-q1.adeo.no/k9/web"
     val k9sakUrlProd = "https://app.adeo.no/k9/web"
-    val refreshUrlDev = "wss://k9-los-oidc-auth-proxy.dev.adeo.no/ws/k9-los-api"
-    val refreshUrlProd = "wss://k9-los-oidc-auth-proxy.nais.adeo.no/ws/k9-los-api"
+    val refreshUrlDev = "wss://k9-los-oidc-auth-proxy.dev.intern.nav.no/ws/k9-los-api"
+    val refreshUrlProd = "wss://k9-los-oidc-auth-proxy.intern.nav.no/ws/k9-los-api"
     val refreshUrlLocal = "ws://localhost:8020/ws"
     val k9punsjUrlDev = "https://k9-punsj-frontend.dev.adeo.no/journalpost"
     val k9punsjUrlProd = "https://k9-punsj-frontend.nais.adeo.no/journalpost"
