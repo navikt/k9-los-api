4360f060e8467ab54859cbd934754669c0306e2b
Frode Lindås <frodeli@gmail.com>
2021-10-18 09:19:04 +0200

    Forbedret overstyring. Kan nå reserver på ident, og med en begrunnelse. (#517)

--- src/main/kotlin/no/nav/k9/tjenester/saksbehandler/oppgave/OppgaveIdMedOverstyring.kt
+++ src/main/kotlin/no/nav/k9/tjenester/saksbehandler/oppgave/OppgaveIdMedOverstyring.kt
@@ -2,5 +2,7 @@ package no.nav.k9.tjenester.saksbehandler.oppgave
 
 data class OppgaveIdMedOverstyring(
     val oppgaveId: String,
-    val overstyrSjekk: Boolean? = false
+    val overstyrSjekk: Boolean = false,
+    val overstyrIdent: String? = null,
+    val overstyrBegrunnelse: String? = null
 )
