150a3dacce471fa02464a4c0b6d673ac66ce35ee
Ole Jørgen Rishoff <olejri@gmail.com>
2021-10-21 10:30:13 +0200

    Fjerner script

--- src/main/resources/migreringer/V1.0_0024.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE nye_og_ferdigstilte DROP CONSTRAINT nye_og_ferdigstilte_pkey;
-
-ALTER TABLE nye_og_ferdigstilte
-    DROP COLUMN IF EXISTS kilde;
-
-ALTER TABLE nye_og_ferdigstilte ADD PRIMARY KEY (behandlingType, fagsakYtelseType, dato);
-
-ALTER TABLE nye_og_ferdigstilte add unique (behandlingType, fagsakYtelseType, dato);
