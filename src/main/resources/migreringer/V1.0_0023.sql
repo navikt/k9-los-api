fdcd907bfbac3300a63bc1f4ac7d627b6f9476ac
Ole Jørgen Rishoff <olejri@gmail.com>
2021-10-25 09:19:08 +0200

    Sletter script som bare ble kjørt ut i dev (fjernet også fra q databasen)

--- src/main/resources/migreringer/V1.0_0023.sql
+++
@@ -1,7 +0,0 @@
-alter table nye_og_ferdigstilte add column kilde VARCHAR(100) NOT NULL default 'K9SAK';
-
-ALTER TABLE nye_og_ferdigstilte DROP CONSTRAINT nye_og_ferdigstilte_pkey;
-
-ALTER TABLE nye_og_ferdigstilte ADD PRIMARY KEY (behandlingType, fagsakYtelseType, dato, kilde);
-
-ALTER TABLE nye_og_ferdigstilte add unique (behandlingType, fagsakYtelseType, dato, kilde);
