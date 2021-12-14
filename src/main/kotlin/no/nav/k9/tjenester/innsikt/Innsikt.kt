11a54def8c2de20427cad6ebfef8bc475fdd1e17
Ole Jørgen Rishoff <olejri@gmail.com>
2021-12-14 09:49:01 +0100

    henter ut eventer på gitt saksnummer

--- src/main/kotlin/no/nav/k9/tjenester/innsikt/Innsikt.kt
+++ src/main/kotlin/no/nav/k9/tjenester/innsikt/Innsikt.kt
@@ -102,18 +102,21 @@ fun Route.innsiktGrensesnitt() {
                             }
                         }
                     }
-                    val oppgaveMedId = oppgaveRepository.hentOppgaverSomMatcherSaksnummer("1DMGFC6")
+                    val oppgaveMedId = oppgaveRepository.hentOppgaverSomMatcherSaksnummer("AMHD8")
                     if (oppgaveMedId.isNotEmpty()) {
                         val sortedByDescending = oppgaveMedId.sortedByDescending { it.oppgave.eventTid }
-                        val sakModell = behandlingProsessEventK9Repository.hent(sortedByDescending.first().id)
-                        sakModell.eventer.forEach { behandlingProsessEventDto ->
-                            val stringBuilder = StringBuilder()
-                            behandlingProsessEventDto.aksjonspunktKoderMedStatusListe.map { "kode=" + it.key + ", verdi=" + it.value }
-                                .forEach { stringBuilder.append(it) }
 
-                            div {
-                                classes = setOf("input-group-text display-4")
-                                +"EventTid=${behandlingProsessEventDto.eventTid}, Aksjonspunkter=$stringBuilder"
+                        sortedByDescending.forEach { oppgaveMedId1 ->
+                            val sakModell = behandlingProsessEventK9Repository.hent(oppgaveMedId1.id)
+                            sakModell.eventer.forEach { behandlingProsessEventDto ->
+                                val stringBuilder = StringBuilder()
+                                behandlingProsessEventDto.aksjonspunktKoderMedStatusListe.map { "kode=" + it.key + ", verdi=" + it.value }
+                                    .forEach { stringBuilder.append(it) }
+
+                                div {
+                                    classes = setOf("input-group-text display-4")
+                                    +"BId=${oppgaveMedId1.oppgave.eksternId} EventTid=${behandlingProsessEventDto.eventTid}, Aksjonspunkter=$stringBuilder"
+                                }
                             }
                         }
                     }
