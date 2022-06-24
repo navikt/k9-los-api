903f0dcb875a2cdb14c4a653d19525d2f67b9ae9
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2022-06-24 19:22:43 +0200

    ny versjon av k9-sak kontrakt med ny aksjonspunktkode

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val flywayVersion = "8.5.13"
 val vaultJdbcVersion = "1.3.9"
 val koinVersion = "2.2.3"
 val kotliqueryVersion = "1.8.0"
-val k9SakVersion = "3.3.4"
+val k9SakVersion = "3.3.6"
 val fuelVersion = "2.3.1"
 
 val dusseldorfKtorVersion = "3.1.6.8-1a4651d"
