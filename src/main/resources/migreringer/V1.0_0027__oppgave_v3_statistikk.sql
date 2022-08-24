36fbc563d36ec5fa376e2b5f3dbc6fd926ff4e94
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2022-08-24 12:20:46 +0200

    Foreign keys til migreringsscript

--- src/main/resources/migreringer/V1.0_0027__oppgave_v3_statistikk.sql
+++ src/main/resources/migreringer/V1.0_0027__oppgave_v3_statistikk.sql
@@ -10,8 +10,10 @@ CREATE TABLE if not exists DATATYPE
     ekstern_id                  VARCHAR(100)                            NOT NULL unique,
     omrade_id                   BIGINT                                  NOT NULL, --eier
     liste_type                  boolean                                 NOT NULL,
-    implementasjonstype         VARCHAR(100)                            NOT NULL
+    implementasjonstype         VARCHAR(100)                            NOT NULL,
     --vis_til_bruker              boolean                                 NOT NULL DEFAULT TRUE --ikke aktuelt for statistikk
+    CONSTRAINT FK_DATATYPE_01
+        FOREIGN KEY(omrade_id) references OMRADE(id)
 );
 
 CREATE TABLE if not exists OPPGAVETYPE
@@ -19,8 +21,10 @@ CREATE TABLE if not exists OPPGAVETYPE
     id                          BIGINT GENERATED ALWAYS AS IDENTITY     NOT NULL PRIMARY KEY,
     ekstern_id                  VARCHAR(100)                            NOT NULL unique,
     omrade_id                   BIGINT                                  NOT NULL,
-    definisjonskilde            VARCHAR(100)                            NOT NULL
+    definisjonskilde            VARCHAR(100)                            NOT NULL,
     --offentlig                   boolean                                 NOT NULL DEFAULT FALSE
+    CONSTRAINT FK_OPPGAVETYPE_01
+        FOREIGN KEY(omrade_id) REFERENCES OMRADE(id)
 );
 
 CREATE TABLE IF NOT EXISTS OPPGAVEFELT
@@ -28,11 +32,15 @@ CREATE TABLE IF NOT EXISTS OPPGAVEFELT
     id                          BIGINT GENERATED ALWAYS AS IDENTITY     NOT NULL PRIMARY KEY,
     datatype_id                 BIGINT                                  NOT NULL,
     oppgavetype_id              BIGINT                                  NOT NULL,
-    --vis_pa_oppgave              boolean                                 NOT NULL DEFAULT TRUE, --ikke aktuelt for statistikk
-    pakrevd                     boolean                                 NOT NULL DEFAULT FALSE
+    --vis_pa_oppgave              boolean                                 NOT NULL DEFAULT TRUE, --ikke aktuelt for statistikk,
+    pakrevd                     boolean                                 NOT NULL DEFAULT FALSE,
+    CONSTRAINT FK_OPPGAVEFELT_01
+        FOREIGN KEY(datatype_id) REFERENCES DATATYPE(id),
+    CONSTRAINT FK_OPPGAVEFELT_02
+        FOREIGN KEY(oppgavetype_id) REFERENCES OPPGAVETYPE(id)
 );
 
-CREATE TABLE if not exists OPPGAVE
+CREATE TABLE if not exists OPPGAVE_V3
 (
     id                          BIGINT GENERATED ALWAYS AS IDENTITY     NOT NULL PRIMARY KEY,
     ekstern_id                  VARCHAR(100)                            NOT NULL unique,
@@ -40,7 +48,9 @@ CREATE TABLE if not exists OPPGAVE
     versjon                     int                                     NOT NULL,
     aktiv                       boolean                                 NOT NULL DEFAULT TRUE,
     opprettet                   timestamp(3)                            NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    fullfort                    timestamp(3)
+    fullfort                    timestamp(3),
+    CONSTRAINT FK_OPPGAVE_01
+        FOREIGN KEY(oppgavetype_id) REFERENCES OPPGAVETYPE(id)
 );
 
 CREATE TABLE if not exists OPPGAVEFELT_VERDI
@@ -48,5 +58,9 @@ CREATE TABLE if not exists OPPGAVEFELT_VERDI
     id                          BIGINT GENERATED ALWAYS AS IDENTITY     NOT NULL PRIMARY KEY,
     oppgave_id                  BIGINT                                  NOT NULL,
     oppgavefelt_id              BIGINT                                  NOT NULL,
-    verdi                       VARCHAR(100)                            NOT NULL
+    verdi                       VARCHAR(100)                            NOT NULL,
+    CONSTRAINT FK_OPPGAVEFELT_VERDI_01
+        FOREIGN KEY(oppgave_id) REFERENCES OPPGAVE_V3(id),
+    CONSTRAINT FK_OPPGAVEFELT_VERDI_02
+        FOREIGN KEY(oppgavefelt_id) REFERENCES OPPGAVEFELT(id)
 )
