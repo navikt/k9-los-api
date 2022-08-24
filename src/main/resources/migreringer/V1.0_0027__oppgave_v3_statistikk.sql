8c9654e9caccbb111d48e3976d6833664b8c77fd
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2022-08-24 14:48:06 +0200

    DDL-kommentarer

--- src/main/resources/migreringer/V1.0_0027__oppgave_v3_statistikk.sql
+++ src/main/resources/migreringer/V1.0_0027__oppgave_v3_statistikk.sql
@@ -4,6 +4,9 @@ CREATE TABLE if not exists OMRADE
     ekstern_id                  VARCHAR(100)                            NOT NULL unique
 );
 
+comment on table OMRADE is 'Spesifiserer subdomene for oppgaver';
+comment on column OMRADE.ekstern_id is 'Navn på område, feks "k9"';
+
 CREATE TABLE if not exists DATATYPE
 (
     id                          BIGINT GENERATED ALWAYS AS IDENTITY     NOT NULL PRIMARY KEY,
@@ -16,6 +19,12 @@ CREATE TABLE if not exists DATATYPE
         FOREIGN KEY(omrade_id) references OMRADE(id)
 );
 
+comment on table DATATYPE is 'Spesifiserer lovlige datatyper for et gitt område';
+comment on column DATATYPE.ekstern_id is 'Human readable navn på datatype som også er eksponert eksternt. Feks "k9.saksnummer". Skal prefikses med område-id for å unngå navnekollisjoner/unique constraint.';
+comment on column DATATYPE.liste_type is 'Flagg som bestemmer om datatypen skal deserialiseres til en list eller om det er en enkeltverdi.';
+comment on column DATATYPE.implementasjonstype is 'Hvilken "primitiv" datatype som brukes, feks String, int, Date.';
+--comment on column DATATYPE.vis_til_bruker is 'Flagg som bestemmer om datatypen skal kunne brukes til filtrering og/eller koprioritering, eller om det er et rent infofelt.'
+
 CREATE TABLE if not exists OPPGAVETYPE
 (
     id                          BIGINT GENERATED ALWAYS AS IDENTITY     NOT NULL PRIMARY KEY,
@@ -26,6 +35,11 @@ CREATE TABLE if not exists OPPGAVETYPE
     CONSTRAINT FK_OPPGAVETYPE_01
         FOREIGN KEY(omrade_id) REFERENCES OMRADE(id)
 );
+
+comment on table OPPGAVETYPE is 'Spesifiserer lovlige oppgavetyper for et gitt område.';
+comment on column OPPGAVETYPE.ekstern_id is 'Human readable navn på oppgavetype som også er eksponert eksternt. Feks "k9.aksjonspunkt". Skal prefikses med område-id for å unngå navnekollisjoner/unique constraint.';
+comment on column OPPGAVETYPE.definisjonskilde is 'Referanse som beskriver hvor definisjonen av oppgavetypen kommer fra. I praksis en "eier" av en gitt oppgavetype. Typisk vil dette være domeneadaptere som konverterer eventer fra fagsystemer til oppgaver i los.';
+--comment on column OPPGAVETYPE.offentlig is 'Flagg som bestemmer om andre domeneadaptere enn eier skal få lov til å generere oppgaver av en gitt type';
 
 CREATE TABLE IF NOT EXISTS OPPGAVEFELT
 (
@@ -40,6 +54,10 @@ CREATE TABLE IF NOT EXISTS OPPGAVEFELT
         FOREIGN KEY(oppgavetype_id) REFERENCES OPPGAVETYPE(id)
 );
 
+comment on table OPPGAVEFELT is 'Spesifiserer hvilke definerte datatyper som anvendes på en gitt oppgave.';
+--comment on column OPPGAVEFELT.vis_pa_oppgave is 'Angir om et felt på en oppgave skal være synlig i frontend eller ikke.';
+comment on column OPPGAVEFELT.pakrevd is 'Angir om et felt er påkrevd eller frivillig på den aktuelle oppgaven.';
+
 CREATE TABLE if not exists OPPGAVE_V3
 (
     id                          BIGINT GENERATED ALWAYS AS IDENTITY     NOT NULL PRIMARY KEY,
@@ -53,6 +71,12 @@ CREATE TABLE if not exists OPPGAVE_V3
         FOREIGN KEY(oppgavetype_id) REFERENCES OPPGAVETYPE(id)
 );
 
+comment on table OPPGAVE_V3 is 'Konkrete oppgaver av en definert oppgavetype';
+comment on column OPPGAVE_V3.ekstern_id is 'TODO'; ------------------------------------------------------------------
+comment on column OPPGAVE_V3.versjon is 'TOOD'; ---------------------------------------------------------------------
+comment on column OPPGAVE_V3.aktiv is 'Flagg som angir om en oppgave er løst eller ikke';
+
+
 CREATE TABLE if not exists OPPGAVEFELT_VERDI
 (
     id                          BIGINT GENERATED ALWAYS AS IDENTITY     NOT NULL PRIMARY KEY,
@@ -63,4 +87,7 @@ CREATE TABLE if not exists OPPGAVEFELT_VERDI
         FOREIGN KEY(oppgave_id) REFERENCES OPPGAVE_V3(id),
     CONSTRAINT FK_OPPGAVEFELT_VERDI_02
         FOREIGN KEY(oppgavefelt_id) REFERENCES OPPGAVEFELT(id)
-)
+);
+
+comment on table OPPGAVEFELT_VERDI is 'Konkrete verdier på en oppgave, som predefinert i OPPGAVEFELT.';
+comment on column OPPGAVEFELT_VERDI.verdi is 'Verdiens egenskaper bestemmes av tabellen DATATYPE via fremmednøkkel i oppgavefelt.'
