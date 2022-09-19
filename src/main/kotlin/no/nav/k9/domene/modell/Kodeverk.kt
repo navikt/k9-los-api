ce613c6fad3806096d05692e0a87570b2a6e1290
Georg Persen <georg.f.persen@gmail.com>
2022-09-19 15:33:56 +0200

    Legger til gruppering av OppgaveKode (aksjonspunkt) (#1068)
    * Legger til gruppering av OppgaveKode (aksjonspunkt)
    
    * Endrer gruppering i OppgaveKode til å bruke enum.navn for å unngå skrivefeil i grupperinger

--- src/main/kotlin/no/nav/k9/domene/modell/Kodeverk.kt
+++ src/main/kotlin/no/nav/k9/domene/modell/Kodeverk.kt
@@ -175,17 +175,45 @@ enum class BehandlingType(override val kode: String, override val navn: String, 
 }
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-enum class OppgaveKode(override val kode: String, override val navn: String) : Kodeverdi {
-    SYKDOM("9001", "Sykdom"),
-    INFOTRYGD_SØK("9007", "Infotrygdsøk"),
-    OMSORG("9020", "Omsorg"),
-    ETTERLYS_INNTEKTSMELDING("9068", "Etterlys inntektsmelding"),
-    AVKLAR_MANGLENDE_INNTEKTSMELDING("9069", "Avklar manglende inntektsmelding"),
-    FASTSETT_BEREGNINGSGRUNNLAG("5038", "Fastsett beregningsgrunnlag"),
-    MEDLEMSKAP("5053", "Medlemskap"),
-    FORESLÅ_VEDTAK("5015", "Forslå vedtak"),
-    FORESLÅ_VEDTAK_MANUELT("5028", "Foreslå vedtak manuelt"),
-    FATTE_VEDTAK("5016", "Fatte vedtak");
+enum class OppgaveKode(override val kode: String, override val navn: String, val gruppering: String) : Kodeverdi {
+    // Innledene behandling
+    MEDLEMSKAP("5053", "Medlemskap", OppgaveKodeGruppe.INNLEDENDE_BEHANDLING.navn),
+    SØKNADSFRIST("5077", "Søknadsfrist", OppgaveKodeGruppe.INNLEDENDE_BEHANDLING.navn),
+    OPPTJENING("5089", "Opptjening", OppgaveKodeGruppe.INNLEDENDE_BEHANDLING.navn),
+    SYKDOM("5053", "Sykdom", OppgaveKodeGruppe.INNLEDENDE_BEHANDLING.navn),
+    OMSORGEN_FOR("5053", "Omsorgen for", OppgaveKodeGruppe.INNLEDENDE_BEHANDLING.navn),
+    AVKLAR_VERGE("5053", "Avklar verge", OppgaveKodeGruppe.INNLEDENDE_BEHANDLING.navn),
+    // Om barnet
+    NATTEVÅK("9200", "Nattevåk", OppgaveKodeGruppe.OM_BARNET.navn),
+    BEREDSKAP("9201", "Beredskap", OppgaveKodeGruppe.OM_BARNET.navn),
+    BARNS_DØD("9202", "Barns død", OppgaveKodeGruppe.OM_BARNET.navn),
+    // Mangler inntektsmelding
+    AVKLAR_MANGLENDE_IM("9069", "Avklar manglende IM", OppgaveKodeGruppe.MANGLER_INNTEKTSMELDING.navn),
+    ENDELIG_AVKLARING_MANGLER_IM("9071", "Endelig avklaring mangler IM", OppgaveKodeGruppe.MANGLER_INNTEKTSMELDING.navn),
+    MANGLER_ARBEIDSTID("9203", "Mangler arbeidstid", OppgaveKodeGruppe.MANGLER_INNTEKTSMELDING.navn),
+    // Beregning
+    FASTSETT_BEREGNINGSGRUNNLAG("5038", "Fastsett beregningsgrunnlag", OppgaveKodeGruppe.BEREGNING.navn),
+    NY_ENDRET_SN_VARIG_ENDRING("5039", "Ny/endret SN (varig endring)", OppgaveKodeGruppe.BEREGNING.navn),
+    NY_ENDRET_SN_NY_I_ARB_LIVET("5049", "Ny/endret SN (ny i arb.livet)", OppgaveKodeGruppe.BEREGNING.navn),
+    FORDEL_BEREGNINGSGRUNNLAG("5046", "Fordel beregningsgrunnlag", OppgaveKodeGruppe.BEREGNING.navn),
+    TIDSBEGRENSET_ARBEIDSFORHOLD("5047", "Tidsbegrenset arbeidsforhold", OppgaveKodeGruppe.BEREGNING.navn),
+    AKTIVITETER("5052", "Aktiviteter", OppgaveKodeGruppe.BEREGNING.navn),
+    BEREGNINGSFAKTA("5058", "Beregningsfakta", OppgaveKodeGruppe.BEREGNING.navn),
+    FEILUTBETALING("5084", "Feilutbetaling", OppgaveKodeGruppe.BEREGNING.navn),
+    OVERSTYRING_BEREGNINGSAKTIVITET("6014", "Overstyring beregningsaktivitet", OppgaveKodeGruppe.BEREGNING.navn),
+    OVERSTYRING_BEREGNINGSGRUNNLAG("6015", "Overstyring beregningsgrunnlag", OppgaveKodeGruppe.BEREGNING.navn),
+    // Flyttesaker
+    MANUELL_BEREGNING("9005", "Manuell beregning", OppgaveKodeGruppe.FLYTTESAKER.navn),
+    INFOTRYGDSØKNAD("9007", "Infotrygsøknad", OppgaveKodeGruppe.FLYTTESAKER.navn),
+    INFOTRYGDSØKNAD_TO_PERSONER("9008", "Infotrygdsøknad 2 personer", OppgaveKodeGruppe.FLYTTESAKER.navn),
+    // Fatte vedtak
+    FORESLÅ_VEDTAK("5015", "Foreslå vedtak", OppgaveKodeGruppe.FATTE_VEDTAK.navn),
+    FORESLÅ_VEDTAK_MANUELT("5028", "Foreslå vedtak manuelt", OppgaveKodeGruppe.FATTE_VEDTAK.navn),
+    VURDERE_ANNEN_YTELSE_FØR_VEDTAK_KODE("5033", "Sjekk VKY", OppgaveKodeGruppe.FATTE_VEDTAK.navn),
+    VURDER_DOKUMENT("5034", "Vurder dokument", OppgaveKodeGruppe.FATTE_VEDTAK.navn),
+    // Uspesifisert
+    KONTROLL_MANUELL_REVURDERING("5056", "Kontroll manuell revurdering", OppgaveKodeGruppe.USPESIFISERT.navn),
+    VURDER_REFUSJON_BERGRUNN_KODE("5059", "Mangler navn", OppgaveKodeGruppe.USPESIFISERT.navn);
 
     override val kodeverk = "OPPGAVE_KODE"
 
@@ -197,6 +225,16 @@ enum class OppgaveKode(override val kode: String, override val navn: String) : K
             return OppgaveKode.values().find { it.kode == kode } ?: throw IllegalStateException("Kjenner ikke igjen koden=$kode")
         }
     }
+}
+
+enum class OppgaveKodeGruppe(val navn: String) {
+    INNLEDENDE_BEHANDLING("Innledende behandling"),
+    OM_BARNET("Om barnet"),
+    MANGLER_INNTEKTSMELDING("Mangler inntektsmelding"),
+    BEREGNING("Beregning"),
+    FLYTTESAKER("Flyttesaker"),
+    FATTE_VEDTAK("Fatte vedtak"),
+    USPESIFISERT("Uspesifisert");
 }
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
