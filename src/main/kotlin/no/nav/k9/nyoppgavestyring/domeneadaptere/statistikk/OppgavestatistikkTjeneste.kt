0313c251c8260ecb0875a6f09e8295dd847f2e32
Georg Persen <georg.f.persen@gmail.com>
2022-09-30 11:07:32 +0200

    Logger tidsbruk for henting av oppgave med historiske versjoner (#1131)

--- src/main/kotlin/no/nav/k9/nyoppgavestyring/domeneadaptere/statistikk/OppgavestatistikkTjeneste.kt
+++ src/main/kotlin/no/nav/k9/nyoppgavestyring/domeneadaptere/statistikk/OppgavestatistikkTjeneste.kt
@@ -2,11 +2,14 @@ package no.nav.k9.nyoppgavestyring.domeneadaptere.statistikk
 
 import kotliquery.TransactionalSession
 import no.nav.k9.nyoppgavestyring.visningoguttrekk.OppgaveRepository
+import org.slf4j.LoggerFactory
 
 class OppgavestatistikkTjeneste(
     private val oppgaveRepository: OppgaveRepository,
     private val statistikkPublisher: StatistikkPublisher
 ) {
+
+    private val log = LoggerFactory.getLogger(OppgavestatistikkTjeneste::class.java)
 
     fun sendStatistikk(id: String, tx: TransactionalSession) {
         val (sak, behandling) = byggOppgavestatistikk(id, tx)
@@ -15,8 +18,10 @@ class OppgavestatistikkTjeneste(
 
     //TODO: koble fra transaksjon? Dette burde ikke kjøre i samme tx som lagring av oppgave, tror jeg
     private fun byggOppgavestatistikk(id: String, tx: TransactionalSession): Pair<Sak, Behandling> {
+        val hentOppgaveMedVersjoner = System.currentTimeMillis()
         val oppgaveMedHistoriskeVersjoner =
             oppgaveRepository.hentOppgaveMedHistoriskeVersjoner(tx, id)
+        log.info("Hentet oppgave med historiske versjoner: $id, tidsbruk: ${System.currentTimeMillis() - hentOppgaveMedVersjoner}")
         val behandling = OppgaveTilBehandlingMapper().lagBehandling(oppgaveMedHistoriskeVersjoner)
         val sak = OppgaveTilSakMapper().lagSak(oppgaveMedHistoriskeVersjoner)
         return Pair(sak, behandling)
