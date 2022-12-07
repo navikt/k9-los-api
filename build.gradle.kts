70f8d4f8a8192a80c12a6485dcc90eb27414feed
qadeer <qadeer.ahmad.khan@nav.no>
2022-12-07 10:16:11 +0100

    oppdaterer kontrakt

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val flywayVersion = "9.6.0"
 val vaultJdbcVersion = "1.3.10"
 val koinVersion = "2.2.3"
 val kotliqueryVersion = "1.9.0"
-val k9SakVersion = "3.3.24"
+val k9SakVersion = "3.3.25"
 val fuelVersion = "2.3.1"
 
 val dusseldorfKtorVersion = "3.1.6.8-1a4651d"
