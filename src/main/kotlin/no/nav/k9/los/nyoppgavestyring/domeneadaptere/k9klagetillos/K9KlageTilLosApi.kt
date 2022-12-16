6f5ae688e6b094fa3f7cf50617ccbbb0cd9f2279
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2022-12-16 11:41:26 +0100

    Korrigert imports

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9klagetillos/K9KlageTilLosApi.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9klagetillos/K9KlageTilLosApi.kt
@@ -1,8 +1,8 @@
 package no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9klagetillos
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import no.nav.k9.los.integrasjon.rest.RequestContextService
 import no.nav.k9.los.nyoppgavestyring.mottak.oppgave.OppgaveV3Tjeneste
 import org.koin.ktor.ext.inject
