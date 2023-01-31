72646bf9fc410df21aeff9a9bccd742899ce0f3b
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2023-01-31 14:49:15 +0100

    Deaktivere restendepunkter i prod, frem til adaptere blir trukket ut av los

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/mottak/feltdefinisjon/FeltdefinisjonApi.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/mottak/feltdefinisjon/FeltdefinisjonApi.kt
@@ -1,26 +1,33 @@
 package no.nav.k9.los.nyoppgavestyring.mottak.feltdefinisjon
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
+import no.nav.k9.los.Configuration
 import no.nav.k9.los.integrasjon.rest.RequestContextService
 import org.koin.ktor.ext.inject
 
 internal fun Route.FeltdefinisjonApi() {
     val requestContextService by inject<RequestContextService>()
     val feltdefinisjonTjeneste by inject<FeltdefinisjonTjeneste>()
+    val config by inject<Configuration>()
 
     //TODO: definere lovlige datatyper -> tolkes_som. enum i koden
 
     post {
-        requestContextService.withRequestContext(call) {
-            val innkommendeFeltdefinisjonerDto = call.receive<FeltdefinisjonerDto>()
+        if (config.nyOppgavestyringRestAktivert()) {
+            requestContextService.withRequestContext(call) {
+                val innkommendeFeltdefinisjonerDto = call.receive<FeltdefinisjonerDto>()
 
-            feltdefinisjonTjeneste.oppdater(innkommendeFeltdefinisjonerDto)
+                feltdefinisjonTjeneste.oppdater(innkommendeFeltdefinisjonerDto)
 
-            call.respond("OK")
+                call.respond("OK")
+            }
+        } else {
+            call.respond(HttpStatusCode.Locked)
         }
     }
 }
