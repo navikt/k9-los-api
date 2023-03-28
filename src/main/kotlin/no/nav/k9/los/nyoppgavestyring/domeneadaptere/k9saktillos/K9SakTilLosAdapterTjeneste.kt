282ef74dd9b6e95bea39a449686ef6983767a800
Stian Grenborgen <stiangre@ifi.uio.no>
2023-03-28 10:57:02 +0200

    Prioriterer autopunkt fremfor andre aksjonspunkt. (#1456)
    * Prioriterer autopunkt fremfor andre aksjonspunkt. Dette fordi k9-sak alltid venter på autopunkt uavhengig av steg.
    
    * Tillater autopunkt uten steg.

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9saktillos/K9SakTilLosAdapterTjeneste.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9saktillos/K9SakTilLosAdapterTjeneste.kt
@@ -20,7 +20,6 @@ import no.nav.k9.los.nyoppgavestyring.mottak.oppgavetype.OppgavetyperDto
 import no.nav.k9.sak.kontrakt.aksjonspunkt.AksjonspunktTilstandDto
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.time.LocalDateTime
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
@@ -306,8 +305,13 @@ class K9SakTilLosAdapterTjeneste(
             return førsteAPMedFristOgVenteårsak.venteårsak.ventekategori
         }
 
-        if (behandlingSteg.isNullOrEmpty()) {
-            throw IllegalStateException("Ikke aktivt behandlingssteg, og mangler aksjonspunkt med ventefrist")
+        val autopunkter = åpneAksjonspunkter.filter { åpentAksjonspunkt ->
+            val aksjonspunktDefinisjon = AksjonspunktDefinisjon.fraKode(åpentAksjonspunkt.aksjonspunktKode)
+            aksjonspunktDefinisjon.erAutopunkt()
+        }
+
+        if (autopunkter.isEmpty() && behandlingSteg.isNullOrEmpty()) {
+            throw IllegalStateException("Ikke aktivt behandlingssteg, og mangler autopunkt eller aksjonspunkt med ventefrist")
         }
 
         val ventekategorierPrioritert = listOf(
@@ -319,14 +323,19 @@ class K9SakTilLosAdapterTjeneste(
             Ventekategori.AVVENTER_ANNET_IKKE_SAKSBEHANDLINGSTID
         )
 
-        //Hvis ingen venteårsak hentes ventekategori fra løsbare aksjonspunkt i prioritert rekkefølge
-        val løsbareAksjonspunkt = åpneAksjonspunkter.filter { åpentAksjonspunkt ->
-            val aksjonspunktDefinisjon = AksjonspunktDefinisjon.fraKode(åpentAksjonspunkt.aksjonspunktKode)
-            aksjonspunktDefinisjon.erAutopunkt() || aksjonspunktDefinisjon.behandlingSteg != null && aksjonspunktDefinisjon.behandlingSteg.kode == behandlingSteg
+        // Prioriter autopunkter fremfor andre aksjonspunkter
+        ventekategorierPrioritert.forEach { ventekategori ->
+            if (apInneholder(autopunkter, ventekategori)) {
+                return ventekategori
+            }
         }
 
+        val løsbareManuelleAksjonspunkter = åpneAksjonspunkter.filter { åpentAksjonspunkt ->
+            val aksjonspunktDefinisjon = AksjonspunktDefinisjon.fraKode(åpentAksjonspunkt.aksjonspunktKode)
+            aksjonspunktDefinisjon.behandlingSteg != null && aksjonspunktDefinisjon.behandlingSteg.kode == behandlingSteg
+        }
         ventekategorierPrioritert.forEach { ventekategori ->
-            if (apInneholder(løsbareAksjonspunkt, ventekategori)) {
+            if (apInneholder(løsbareManuelleAksjonspunkter, ventekategori)) {
                 return ventekategori
             }
         }
