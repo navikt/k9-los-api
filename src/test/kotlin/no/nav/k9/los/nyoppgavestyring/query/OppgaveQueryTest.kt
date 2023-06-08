ced86b42ecf35dab427b410f148fdfe5f7401f16
Stian Grenborgen <stian.grenborgen@nav.no>
2023-06-08 15:08:37 +0200

    Legger til støtte for å deserialisere feltverdier som ikke er i array. Dette fordi frontend allerede sender dette inn.

--- src/test/kotlin/no/nav/k9/los/nyoppgavestyring/query/OppgaveQueryTest.kt
+++ src/test/kotlin/no/nav/k9/los/nyoppgavestyring/query/OppgaveQueryTest.kt
@@ -2,6 +2,7 @@ package no.nav.k9.los.nyoppgavestyring.query
 
 import assertk.assertThat
 import assertk.assertions.isEmpty
+import assertk.assertions.isNotNull
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
@@ -50,6 +51,31 @@ class OppgaveQueryTest : AbstractK9LosIntegrationTest() {
 
         val result = oppgaveQueryRepository.query(oppgaveQuery)
         assertThat(result).isEmpty()
+    }
+
+    @Test
+    fun `sjekker at oppgave-query kan deserialiseres med feltverdi som ikke er array`() {
+        val json = """
+            {
+              "filtere" : [{
+                "type" : "feltverdi",
+                "område" : null,
+                "kode" : "oppgavestatus",
+                "operator" : "EQUALS",
+                "verdi" : "OPPR"
+              }],
+              "select" : [ ],
+              "order" : [ ],
+              "limit" : 10
+            }
+        """.trimIndent()
+
+        val om = ObjectMapper().dusseldorfConfigured()
+            .enable(SerializationFeature.INDENT_OUTPUT)
+            .registerKotlinModule()
+
+        val objectAgain: OppgaveQuery = om.readValue(json, OppgaveQuery::class.java)
+        assertThat(objectAgain).isNotNull()
     }
 
 }
