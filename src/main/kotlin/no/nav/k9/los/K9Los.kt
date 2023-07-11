fe8d616142ce26a9344b2e93d74e654fc7e93a63
Stian Grenborgen <stiangre@ifi.uio.no>
2023-07-11 22:01:54 +0200

    Kjører feilvaskejobb.

--- src/main/kotlin/no/nav/k9/los/K9Los.kt
+++ src/main/kotlin/no/nav/k9/los/K9Los.kt
@@ -99,7 +99,7 @@ fun Application.k9Los() {
     val k9KlageTilLosAdapterTjeneste = koin.get<K9KlageTilLosAdapterTjeneste>()
     k9KlageTilLosAdapterTjeneste.setup()
 
-    if (LocalDateTime.now().isBefore(LocalDateTime.of(2023, 6, 28, 20, 30))) {
+    if (LocalDateTime.now().isBefore(LocalDateTime.of(2023, 7, 11, 22, 15))) {
         koin.get<K9SakTilLosLukkeFeiloppgaverTjeneste>().kjørFeiloppgaverVask()
         //koin.get<K9SakTilLosHistorikkvaskTjeneste>().kjørHistorikkvask()
         //koin.get<K9KlageTilLosHistorikkvaskTjeneste>().kjørHistorikkvask()
