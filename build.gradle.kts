049e3b327db19a7bc0d473b96670578a2b5ade2e
Bjornar Askevold <bjornar.askevold@nav.no>
2023-12-05 15:49:24 +0100

    Bumper dusseldorfKtorVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ val fuelVersion = "2.3.1"
 val jacksonVersion = "2.16.0"
 val commonsTextVersion = "1.11.0"
 
-val dusseldorfKtorVersion = "4.1.1"
+val dusseldorfKtorVersion = "4.1.2"
 val ktorVersion = "2.3.6"
 val kafkaVersion = "3.6.0"
 
