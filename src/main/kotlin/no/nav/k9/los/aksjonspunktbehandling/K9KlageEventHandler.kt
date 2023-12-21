94e153c87558543950f307e1064db0d2a19f3dfc
Bjornar Askevold <bjornar.askevold@nav.no>
2023-12-21 11:26:32 +0100

    Vaskeeventer fra k9-klage

--- src/main/kotlin/no/nav/k9/los/aksjonspunktbehandling/K9KlageEventHandler.kt
+++ src/main/kotlin/no/nav/k9/los/aksjonspunktbehandling/K9KlageEventHandler.kt
@@ -1,6 +1,9 @@
 package no.nav.k9.los.aksjonspunktbehandling
 
+import no.nav.k9.klage.kodeverk.behandling.BehandlingResultatType
+import no.nav.k9.klage.kodeverk.behandling.oppgavetillos.EventHendelse
 import no.nav.k9.klage.kontrakt.behandling.oppgavetillos.KlagebehandlingProsessHendelse
+import no.nav.k9.los.domene.modell.BehandlingStatus
 import no.nav.k9.los.domene.modell.FagsakYtelseType
 import no.nav.k9.los.domene.modell.K9KlageModell
 import no.nav.k9.los.domene.repository.BehandlingProsessEventKlageRepository
@@ -25,8 +28,11 @@ class K9KlageEventHandler constructor(
     )
 
     fun prosesser(
-        event: KlagebehandlingProsessHendelse
+        eventInn: KlagebehandlingProsessHendelse
     ) {
+        val event = håndterVaskeevent(eventInn)
+        if (event == null) return
+
         behandlingProsessEventKlageRepository.lagre(event.eksternId!!) { k9KlageModell ->
             if (k9KlageModell == null) {
                 return@lagre K9KlageModell(mutableListOf(event))
@@ -39,5 +45,52 @@ class K9KlageEventHandler constructor(
             k9KlageModell
         }
         k9KlageTilLosAdapterTjeneste.oppdaterOppgaveForBehandlingUuid(event.eksternId)
+    }
+
+    fun håndterVaskeevent(event: KlagebehandlingProsessHendelse): KlagebehandlingProsessHendelse? {
+        if (event.eventHendelse != EventHendelse.VASKEEVENT) {
+            return event
+        }
+
+        // Gjøres utenfor transaksjon fordi den ikke muterer data.
+        // Det er ikke mulig å unngå lagring selv om eventet skal ignoreres hvis den skulle ha vært i samme transaksjon (pga on conflict(id) update.. ) i lagre-metoden
+        val eksisterendeEventModell = behandlingProsessEventKlageRepository.hent(event.eksternId!!)
+        if (eksisterendeEventModell.eventer.any { tidligereEvent -> tidligereEvent.behandlingStatus == BehandlingStatus.AVSLUTTET.kode }) {
+            return null
+        }
+
+        if (eksisterendeEventModell.eventer.isEmpty()) {
+            log.info("Vaskeeventfiltrering gjelder behandling som ikke tidligere finnes i los ${event.eksternId}")
+            return event
+        }
+
+        log.info("Vaskeeventfiltrering ${event.behandlingStatus} - ${event.eventTid} - ${event.eksternId}")
+        return eksisterendeEventModell.eventer.last().eventTid
+            .takeIf { sisteEventTid -> sisteEventTid.isAfter(event.eventTid) }
+            ?.let { sisteEventTid -> KlagebehandlingProsessHendelse.builder()
+                .medEksternId(event.eksternId)
+                .medPåklagdBehandlingEksternId(event.påklagdBehandlingEksternId)
+                .medFagsystem(event.fagsystem)
+                .medSaksnummer(event.saksnummer)
+                .medAktørId(event.aktørId)
+                .medEventTid(sisteEventTid.plusNanos(100_1000))
+                .getBehandlingstidFrist(event.behandlingstidFrist)
+                .medEventHendelse(event.eventHendelse)
+                .medBehandlingStatus(event.behandlingStatus)
+                .medBehandlingSteg(event.behandlingSteg)
+                .medBehandlendeEnhet(event.behandlendeEnhet)
+                .medYtelseTypeKode(event.ytelseTypeKode)
+                .medBehandlingResultat(event.resultatType)
+                .medBehandlingTypeKode(event.behandlingTypeKode)
+                .medOpprettetBehandling(event.opprettetBehandling)
+                .medAnsvarligSaksbehandler(event.ansvarligSaksbehandler)
+                .medFagsakPeriode(event.fagsakPeriode)
+                .medPleietrengendeAktørId(event.pleietrengendeAktørId)
+                .medRelatertPartAktørId(event.relatertPartAktørId)
+                .medAnsvarligBeslutter(event.ansvarligBeslutter)
+                .medAksjonspunktTilstander(event.aksjonspunkttilstander)
+                .medVedtaksdato(event.vedtaksdato)
+                .build()
+            } ?: event
     }
 }
