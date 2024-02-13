6e34853c1440b3bc7db70fe58fec2998c4620fdb
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2024-02-13 16:25:36 +0100

    Historikkvask i Q

--- src/main/kotlin/no/nav/k9/los/K9Los.kt
+++ src/main/kotlin/no/nav/k9/los/K9Los.kt
@@ -98,12 +98,12 @@ fun Application.k9Los() {
     val k9KlageTilLosAdapterTjeneste = koin.get<K9KlageTilLosAdapterTjeneste>()
     k9KlageTilLosAdapterTjeneste.setup()
 
-    if (LocalDateTime.now().isBefore(LocalDateTime.of(2024, 1, 17, 18, 0))) {
+    if (LocalDateTime.now().isBefore(LocalDateTime.of(2024, 2, 13, 17, 0))) {
         //koin.get<K9SakTilLosLukkeFeiloppgaverTjeneste>().kjørFeiloppgaverVask()
-        //koin.get<K9SakTilLosHistorikkvaskTjeneste>().kjørHistorikkvask()
+        koin.get<K9SakTilLosHistorikkvaskTjeneste>().kjørHistorikkvask()
         //koin.get<K9KlageTilLosHistorikkvaskTjeneste>().kjørHistorikkvask()
         //koin.get<OppgavestatistikkTjeneste>().slettStatistikkgrunnlag()
-        koin.get<ReservasjonKonverteringJobb>().kjørReservasjonskonvertering()
+        //koin.get<ReservasjonKonverteringJobb>().kjørReservasjonskonvertering()
     }
 
     install(Authentication) {
