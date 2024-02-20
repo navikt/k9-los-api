34a715f07f22c758a20f45786e1f1c0653e9cf08
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2024-02-20 14:46:00 +0100

    Historikkvask take 3

--- src/main/kotlin/no/nav/k9/los/K9Los.kt
+++ src/main/kotlin/no/nav/k9/los/K9Los.kt
@@ -99,7 +99,7 @@ fun Application.k9Los() {
     val k9KlageTilLosAdapterTjeneste = koin.get<K9KlageTilLosAdapterTjeneste>()
     k9KlageTilLosAdapterTjeneste.setup()
 
-    if (LocalDateTime.now().isBefore(LocalDateTime.of(2024, 2, 20, 14, 45))) {
+    if (LocalDateTime.now().isBefore(LocalDateTime.of(2024, 2, 20, 15, 30))) {
         //koin.get<K9SakTilLosLukkeFeiloppgaverTjeneste>().kjørFeiloppgaverVask()
         koin.get<K9SakTilLosHistorikkvaskTjeneste>().kjørHistorikkvask()
         koin.get<K9KlageTilLosHistorikkvaskTjeneste>().kjørHistorikkvask()
