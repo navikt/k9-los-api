0767094ad60e452264b293b30c57d21baa1514ba
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2024-02-20 11:28:46 +0100

    Kjøre historikkvask i Q

--- src/main/kotlin/no/nav/k9/los/K9Los.kt
+++ src/main/kotlin/no/nav/k9/los/K9Los.kt
@@ -44,6 +44,7 @@ import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.saktillos.K9SakTilLosAda
 import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.saktillos.K9SakTilLosApi
 import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.statistikk.OppgavestatistikkTjeneste
 import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.statistikk.StatistikkApi
+import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9klagetillos.K9KlageTilLosHistorikkvaskTjeneste
 import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9saktillos.K9SakTilLosHistorikkvaskTjeneste
 import no.nav.k9.los.nyoppgavestyring.ko.OppgaveKoApis
 import no.nav.k9.los.nyoppgavestyring.mottak.feltdefinisjon.FeltdefinisjonApi
@@ -98,10 +99,10 @@ fun Application.k9Los() {
     val k9KlageTilLosAdapterTjeneste = koin.get<K9KlageTilLosAdapterTjeneste>()
     k9KlageTilLosAdapterTjeneste.setup()
 
-    if (LocalDateTime.now().isBefore(LocalDateTime.of(2024, 2, 13, 17, 0))) {
+    if (LocalDateTime.now().isBefore(LocalDateTime.of(2024, 2, 20, 12, 30))) {
         //koin.get<K9SakTilLosLukkeFeiloppgaverTjeneste>().kjørFeiloppgaverVask()
         koin.get<K9SakTilLosHistorikkvaskTjeneste>().kjørHistorikkvask()
-        //koin.get<K9KlageTilLosHistorikkvaskTjeneste>().kjørHistorikkvask()
+        koin.get<K9KlageTilLosHistorikkvaskTjeneste>().kjørHistorikkvask()
         //koin.get<OppgavestatistikkTjeneste>().slettStatistikkgrunnlag()
         //koin.get<ReservasjonKonverteringJobb>().kjørReservasjonskonvertering()
     }
