cb113fc64be089fc37b5604ab4367a567f625e4a
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2024-04-09 14:49:48 +0200

    Filtrere bort oppgaver som står på vent fra listen over åpne oppgaver

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/visningoguttrekk/OppgaveRepository.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/visningoguttrekk/OppgaveRepository.kt
@@ -39,11 +39,11 @@ class OppgaveRepository(
                 from oppgave_v3 ov 
                 where reservasjonsnokkel = :reservasjonsnokkel
                 and aktiv = true
-                and status != :oppgavestatus
+                and status = :oppgavestatus
             """.trimIndent(),
                 mapOf(
                     "reservasjonsnokkel" to reservasjonsnøkkel,
-                    "oppgavestatus" to Oppgavestatus.LUKKET.kode,
+                    "oppgavestatus" to Oppgavestatus.AAPEN.kode,
                 )
             ).map { row ->
                 mapOppgave(row, now, tx)
