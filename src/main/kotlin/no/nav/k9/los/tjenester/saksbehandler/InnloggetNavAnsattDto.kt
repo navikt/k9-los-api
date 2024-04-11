d65419a42c0891bce15dd8433639c3aa0f72eb0f
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-04-11 15:10:28 +0200

    Fjernet ubrukte, misvisende, felter fra NavAnsattApis

--- src/main/kotlin/no/nav/k9/los/tjenester/saksbehandler/InnloggetNavAnsattDto.kt
+++ src/main/kotlin/no/nav/k9/los/tjenester/saksbehandler/InnloggetNavAnsattDto.kt
@@ -5,11 +5,6 @@ data class InnloggetNavAnsattDto(
     val navn: String,
     val brukerIdent: String?,
     val kanSaksbehandle: Boolean,
-    val kanVeilede: Boolean,
-    val kanBeslutte: Boolean,
-    val kanBehandleKodeEgenAnsatt: Boolean,
-    val kanBehandleKode6: Boolean,
-    val kanBehandleKode7: Boolean,
     val kanOppgavestyre: Boolean,
     val kanReservere: Boolean,
     val kanDrifte: Boolean
