a9efe1824b76d8f716f4beb6dad910ddca5b961f
Bjørnar Askevold <43171873+baskevold@users.noreply.github.com>
2024-05-28 15:09:11 +0200

    Gjør alle felter i error-response nullable. Bedre å håndtere feil enn å feile på derserialisering av feilreponse (#2184)

--- src/main/kotlin/no/nav/k9/los/integrasjon/pdl/Error.kt
+++ src/main/kotlin/no/nav/k9/los/integrasjon/pdl/Error.kt
@@ -6,18 +6,18 @@ data class Error(
 ) {
     data class Error(
         val extensions: Extensions?,
-        val locations: List<Location>,
-        val message: String,
-        val path: List<String>
+        val locations: List<Location>?,
+        val message: String?,
+        val path: List<String>?
     ) {
         data class Extensions(
-            val classification: String,
-            val code: String
+            val classification: String?,
+            val code: String?
         )
 
         data class Location(
-            val column: Int,
-            val line: Int
+            val column: Int?,
+            val line: Int?
         )
     }
 }
