05c25c0e561903f8e61bf6c03d833dcba6bce324
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-06-22 14:37:01 +0200

    Unngå upsert mot oppgave_v3_aktiv for å unngå å plukke fra sekvens ved oppdatering

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/mottak/oppgave/AktivOppgaveRepository.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/mottak/oppgave/AktivOppgaveRepository.kt
@@ -6,20 +6,28 @@ import no.nav.k9.los.nyoppgavestyring.mottak.oppgavetype.Oppgavetype
 import org.jetbrains.annotations.VisibleForTesting
 
 object AktivOppgaveRepository {
+
     fun ajourholdAktivOppgave(oppgave: OppgaveV3, nyVersjon: Long, tx: TransactionalSession): Long {
-        val oppgaveId =  tx.updateAndReturnGeneratedKey(
+        val oppgaveId = if (nyVersjon == 0L) {
+            opprettOppgaveV3Aktiv(tx, oppgave, nyVersjon)
+        } else {
+            oppdaterOppgaveV3Aktiv(tx, oppgave, nyVersjon)
+        }
+        oppdaterAktivOppgavefelter(oppgaveId, oppgave, nyVersjon, tx)
+        return oppgaveId
+    }
+
+    private fun opprettOppgaveV3Aktiv(
+        tx: TransactionalSession,
+        oppgave: OppgaveV3,
+        nyVersjon: Long
+    ): Long {
+        return tx.updateAndReturnGeneratedKey(
             queryOf(
                 """
-                    insert into oppgave_v3_aktiv (ekstern_id, ekstern_versjon, oppgavetype_id, status, versjon, kildeomrade, endret_tidspunkt, reservasjonsnokkel)
-                    values(:eksternId, :eksternVersjon, :oppgavetypeId, cast(:status as oppgavestatus), :versjon, :kildeomrade, :endretTidspunkt, :reservasjonsnokkel)
-                    on conflict (ekstern_id, kildeomrade)
-                    do update set
-                        ekstern_versjon = :eksternVersjon,
-                        status = cast(:status as oppgavestatus),
-                        versjon = :versjon,
-                        endret_tidspunkt = :endretTidspunkt,
-                        reservasjonsnokkel = :reservasjonsnokkel;
-                """.trimIndent(),
+                        insert into oppgave_v3_aktiv (ekstern_id, ekstern_versjon, oppgavetype_id, status, versjon, kildeomrade, endret_tidspunkt, reservasjonsnokkel)
+                        values(:eksternId, :eksternVersjon, :oppgavetypeId, cast(:status as oppgavestatus), :versjon, :kildeomrade, :endretTidspunkt, :reservasjonsnokkel)                   
+                    """.trimIndent(),
                 mapOf(
                     "eksternId" to oppgave.eksternId,
                     "eksternVersjon" to oppgave.eksternVersjon,
@@ -32,9 +40,50 @@ object AktivOppgaveRepository {
                 )
             )
         )!!
+    }
 
-        oppdaterAktivOppgavefelter(oppgaveId, oppgave, nyVersjon, tx)
-        return oppgaveId
+    private fun oppdaterOppgaveV3Aktiv(
+        tx: TransactionalSession,
+        oppgave: OppgaveV3,
+        nyVersjon: Long,
+    ): Long {
+        val id = tx.run(
+            queryOf(
+                """ select id from oppgave_v3_aktiv where ekstern_id = :eksternId and kildeomrade = :kildeomrade """,
+                mapOf(
+                    "eksternId" to oppgave.eksternId,
+                    "kildeomrade" to oppgave.kildeområde
+                )
+            )
+                .map { row -> row.long("id") }.asSingle
+        )!!
+
+        tx.run(
+            queryOf(
+                """
+                update oppgave_v3_aktiv
+                set
+                        ekstern_versjon = :eksternVersjon,
+                        status = cast(:status as oppgavestatus),
+                        versjon = :versjon,
+                        endret_tidspunkt = :endretTidspunkt,
+                        reservasjonsnokkel = :reservasjonsnokkel 
+                where ekstern_id = :eksternId and kildeomrade = :kildeomrade
+                """,
+                mapOf(
+                    "eksternId" to oppgave.eksternId,
+                    "eksternVersjon" to oppgave.eksternVersjon,
+                    "oppgavetypeId" to oppgave.oppgavetype.id,
+                    "status" to oppgave.status.toString(),
+                    "endretTidspunkt" to oppgave.endretTidspunkt,
+                    "versjon" to nyVersjon,
+                    "kildeomrade" to oppgave.kildeområde,
+                    "reservasjonsnokkel" to oppgave.reservasjonsnøkkel,
+                )
+            ).asUpdate
+        )
+
+        return id;
     }
 
     private fun oppdaterAktivOppgavefelter(
@@ -43,7 +92,7 @@ object AktivOppgaveRepository {
         nyVersjon: Long,
         tx: TransactionalSession
     ) {
-        check(nyVersjon >= 0) {"Ny versjon må være 0 eller høyere: $nyVersjon"}
+        check(nyVersjon >= 0) { "Ny versjon må være 0 eller høyere: $nyVersjon" }
 
         val eksisterendeFelter: List<OppgaveFeltverdi> = if (nyVersjon == 0L) emptyList()
         else hentFeltverdier(oppgaveId, oppgave.oppgavetype, tx)
@@ -83,7 +132,7 @@ object AktivOppgaveRepository {
         oppgaveId: Long,
         tx: TransactionalSession
     ) {
-        if (inserts.isEmpty()){
+        if (inserts.isEmpty()) {
             return
         }
         tx.batchPreparedNamedStatement("""
@@ -102,23 +151,23 @@ object AktivOppgaveRepository {
     }
 
     private fun oppdaterFelter(updates: Map<Long, Verdi>, tx: TransactionalSession) {
-        if (updates.isEmpty()){
+        if (updates.isEmpty()) {
             return
         }
         tx.batchPreparedNamedStatement(
             "update oppgavefelt_verdi_aktiv set verdi = :verdi where id = :id",
-            updates.map { mapOf("id" to it.key, "verdi" to it.value.verdi ) }.toList()
+            updates.map { mapOf("id" to it.key, "verdi" to it.value.verdi) }.toList()
         )
     }
 
 
     private fun slettFelter(deletes: Collection<Long>, tx: TransactionalSession) {
-        if (deletes.isEmpty()){
+        if (deletes.isEmpty()) {
             return
         }
         tx.batchPreparedNamedStatement(
             "delete from oppgavefelt_verdi_aktiv where id = :id",
-            deletes.map { mapOf("id" to it ) }.toList()
+            deletes.map { mapOf("id" to it) }.toList()
         )
     }
 
@@ -128,7 +177,7 @@ object AktivOppgaveRepository {
         if (eksisterende.isEmpty()) {
             return DiffResultat(deletes = emptyList(), inserts = nyeVerdier, updates = emptyMap())
         }
-        val eksisterendeVerdier = eksisterende.associate {Pair(Verdi(it.verdi, it.oppgavefelt.id!!), it.id!!) }
+        val eksisterendeVerdier = eksisterende.associate { Pair(Verdi(it.verdi, it.oppgavefelt.id!!), it.id!!) }
         val verdierBeggeSteder = eksisterendeVerdier.keys.intersect(nyeVerdier)
 
         val oppdaterMap: MutableMap<Long, Verdi> = HashMap()
@@ -136,7 +185,7 @@ object AktivOppgaveRepository {
         val kunNye = (nyeVerdier - verdierBeggeSteder).toMutableSet()
 
         for (nyVerdi in ArrayList(kunNye)) {
-            val match = kunEksisterende.entries.firstOrNull { it.key.oppgavefeltId == nyVerdi.oppgavefeltId}
+            val match = kunEksisterende.entries.firstOrNull { it.key.oppgavefeltId == nyVerdi.oppgavefeltId }
             match?.let {
                 val id = it.value
                 oppdaterMap[id] = nyVerdi
@@ -144,10 +193,14 @@ object AktivOppgaveRepository {
                 kunNye.remove(nyVerdi)
             }
         }
-        return DiffResultat(deletes=kunEksisterende.values, inserts=kunNye, updates=oppdaterMap)
+        return DiffResultat(deletes = kunEksisterende.values, inserts = kunNye, updates = oppdaterMap)
     }
 
-    data class DiffResultat(val deletes : Collection<Long>, val inserts : Collection<Verdi>, val updates : Map<Long, Verdi>)
+    data class DiffResultat(
+        val deletes: Collection<Long>,
+        val inserts: Collection<Verdi>,
+        val updates: Map<Long, Verdi>
+    )
 
-    data class Verdi (val verdi: String, val oppgavefeltId: Long)
+    data class Verdi(val verdi: String, val oppgavefeltId: Long)
 }
