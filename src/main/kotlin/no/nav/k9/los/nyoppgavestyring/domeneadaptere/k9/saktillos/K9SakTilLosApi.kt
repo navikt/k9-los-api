0def13071b60f1f1fd747b80e504d25143ff0a34
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2024-06-25 13:01:24 +0200

    Rollback oppgaverepository

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9/saktillos/K9SakTilLosApi.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9/saktillos/K9SakTilLosApi.kt
@@ -4,8 +4,10 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.html.I
 import no.nav.k9.los.Configuration
 import no.nav.k9.los.integrasjon.rest.RequestContextService
+import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9saktillos.K9SakTilLosAktivvaskTjeneste
 import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9saktillos.K9SakTilLosHistorikkvaskTjeneste
 import no.nav.k9.los.nyoppgavestyring.mottak.oppgave.OppgaveV3Tjeneste
 import org.koin.ktor.ext.inject
@@ -14,6 +16,7 @@ internal fun Route.K9SakTilLosApi() {
     val requestContextService by inject<RequestContextService>()
     val k9SakTilLosAdapterTjeneste by inject<K9SakTilLosAdapterTjeneste>()
     val k9SakTilLosHistorikkvaskTjeneste by inject<K9SakTilLosHistorikkvaskTjeneste>()
+    val k9SakTilLosAktivvaskTjeneste by inject<K9SakTilLosAktivvaskTjeneste>()
     val config by inject<Configuration>()
 
     put("/startOppgaveprosessering") {
@@ -30,6 +33,13 @@ internal fun Route.K9SakTilLosApi() {
     put("/startHistorikkvask") {
         requestContextService.withRequestContext(call) {
             k9SakTilLosHistorikkvaskTjeneste.kjørHistorikkvask()
+            call.respond(HttpStatusCode.NoContent)
+        }
+    }
+
+    put("/startAktivvask") {
+        requestContextService.withRequestContext(call) {
+            k9SakTilLosAktivvaskTjeneste.kjørAktivvask()
             call.respond(HttpStatusCode.NoContent)
         }
     }
