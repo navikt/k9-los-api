3fcbe7c0af679e44f926a907d52f96b356f69b3f
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-06-27 09:14:20 +0200

    partiell indeks

--- src/main/resources/migreringer/V1.0_0063__oppgave_v3_indeks.sql
+++ src/main/resources/migreringer/V1.0_0063__oppgave_v3_indeks.sql
@@ -1,2 +1,2 @@
 
-CREATE INDEX idx_oppgave_v3_reservasjonsnokkel_aktiv_status ON oppgave_v3(reservasjonsnokkel, aktiv, status);
+CREATE INDEX idx_oppgave_v3_reservasjonsnokkel_aktiv_status ON oppgave_v3(reservasjonsnokkel, status) where aktiv = true;
