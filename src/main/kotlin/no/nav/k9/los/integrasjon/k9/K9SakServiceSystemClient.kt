06e646eca41b271446e26e3143623625b69f52a8
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-08-12 12:34:08 +0200

    Lagrer tidspunkt k9sak-behandlinger ble refreshet (#2456)
    * Lagrer tidspunkt k9sak-behandlinger ble refreshet
    
    * Oppdateringer etter code review

--- src/main/kotlin/no/nav/k9/los/integrasjon/k9/K9SakServiceSystemClient.kt
+++ src/main/kotlin/no/nav/k9/los/integrasjon/k9/K9SakServiceSystemClient.kt
@@ -3,15 +3,12 @@ package no.nav.k9.los.integrasjon.k9
 import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
 import com.github.kittinunf.fuel.httpPost
 import io.ktor.http.*
-import kotlinx.coroutines.runBlocking
 import no.nav.helse.dusseldorf.ktor.core.Retry
 import no.nav.helse.dusseldorf.ktor.metrics.Operation
 import no.nav.helse.dusseldorf.oauth2.client.AccessTokenClient
 import no.nav.helse.dusseldorf.oauth2.client.CachedAccessTokenClient
 import no.nav.k9.los.Configuration
 import no.nav.k9.los.integrasjon.rest.NavHeaders
-import no.nav.k9.los.utils.Cache
-import no.nav.k9.los.utils.CacheObject
 import no.nav.k9.los.utils.LosObjectMapper
 import no.nav.k9.sak.kontrakt.behandling.BehandlingIdDto
 import no.nav.k9.sak.kontrakt.behandling.BehandlingIdListe
@@ -23,7 +20,8 @@ import java.util.*
 open class K9SakServiceSystemClient constructor(
     val configuration: Configuration,
     val accessTokenClient: AccessTokenClient,
-    val scope: String
+    val scope: String,
+    k9SakBehandlingOppfrisketRepostiory: K9SakBehandlingOppfrisketRepostiory
 ) : IK9SakService {
     private val log = LoggerFactory.getLogger("K9SakService")!!
 
@@ -31,13 +29,12 @@ open class K9SakServiceSystemClient constructor(
     private val url = configuration.k9Url()
     private val scopes = setOf(scope)
 
-    private val cache = Cache<UUID, Boolean>(cacheSize = 10000)
-    private val cacheObjectDuration = Duration.ofHours(12)
+    private val cache = K9SakBehandlingOppfrisketCache(k9SakBehandlingOppfrisketRepostiory)
 
     override suspend fun refreshBehandlinger(behandlingUuid: Collection<UUID>) {
         val nå = LocalDateTime.now()
         log.info("Forespørsel om refresh av ${behandlingUuid.size} behandlinger")
-        val uoppfriskedeBehandlingUuider = behandlingUuid.filterNot { cache.containsKey(it, nå) }
+        val uoppfriskedeBehandlingUuider = cache.filterNotInCache(behandlingUuid)
         log.info("Kommer til å refreshe ${uoppfriskedeBehandlingUuider.size} behandlinger etter sjekk mot cache av oppfriskede behandlinger, i bolker på 100 stykker")
         for (uuids in uoppfriskedeBehandlingUuider.chunked(100)) {
             utførRefreshKallOgOppdaterCache(uuids)
@@ -76,7 +73,7 @@ open class K9SakServiceSystemClient constructor(
 
             result.fold(
                 { success ->
-                    registrerICache(behandlinger, LocalDateTime.now())
+                    cache.registrerBehandlingerOppfrisket(behandlinger)
                     success
                 },
                 { error ->
@@ -85,13 +82,6 @@ open class K9SakServiceSystemClient constructor(
                 }
             )
         }
-    }
-
-    private fun registrerICache(behandingUuid: Collection<UUID>, now: LocalDateTime) {
-        for (uuid in behandingUuid) {
-            cache.set(uuid, CacheObject(true, now.plus(cacheObjectDuration)))
-        }
-        log.info("La til ${behandingUuid.size} behandlinger i cache")
     }
 
 
