2436091bb07845224eb6b768366357525f4d17dd
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-08-14 13:50:40 +0200

    Setter opp opentelemetry for dev, agent og konfig (inspriert fra pensjon-psak) (#2467)

--- src/main/resources/scripts/run.sh
+++ src/main/resources/scripts/run.sh
@@ -23,3 +23,8 @@ then
     done
 fi
 
+#start opentelemetyr-agenten dersom konfigurert
+if [ ! -z "${OTEL_EXPORTER_OTLP_ENDPOINT}" ]; then
+    echo "Configuring attatchment of opentelemetry-agent"
+    JAVA_OPTS="${JAVA_OPTS} -javaagent:/app/opentelemetry-javaagent.jar"
+fi
