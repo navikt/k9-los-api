075f8f488a460235169c0d9361c2030c6581ef2e
Mats Bolstad <mbolstad@gmail.com>
2024-08-26 11:07:22 +0200

    Felt for journalpost-ID (Punsj) (#2537)
    * Felt for journalpostId
    
    * Setter inn punsj-oppgave lokalt

--- src/main/kotlin/no/nav/k9/los/tjenester/mock/localSetup.kt
+++ src/main/kotlin/no/nav/k9/los/tjenester/mock/localSetup.kt
@@ -2,11 +2,18 @@ package no.nav.k9.los.tjenester.mock
 
 import kotlinx.coroutines.runBlocking
 import no.nav.k9.los.KoinProfile
+import no.nav.k9.los.aksjonspunktbehandling.K9punsjEventHandler
 import no.nav.k9.los.domene.modell.Saksbehandler
 import no.nav.k9.los.domene.repository.OppgaveKøRepository
 import no.nav.k9.los.domene.repository.SaksbehandlerRepository
+import no.nav.k9.los.integrasjon.kafka.dto.PunsjEventDto
+import no.nav.k9.los.nyoppgavestyring.mottak.oppgave.Oppgavestatus
+import no.nav.k9.sak.typer.AktørId
+import no.nav.k9.sak.typer.JournalpostId
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.time.LocalDateTime
+import java.util.*
 
 
 val saksbehandlere = listOf(
@@ -39,6 +46,7 @@ val saksbehandlere = listOf(
 object localSetup : KoinComponent {
     val saksbehandlerRepository: SaksbehandlerRepository by inject()
     val oppgaveKøRepository: OppgaveKøRepository by inject()
+    val punsjEventHandler: K9punsjEventHandler by inject()
     val profile: KoinProfile by inject()
 
     fun initSaksbehandlere() {
@@ -53,4 +61,26 @@ object localSetup : KoinComponent {
             }
         }
     }
+
+    fun initPunsjoppgave() {
+        if (profile == KoinProfile.LOCAL) {
+            runBlocking {
+                punsjEventHandler.prosesser(PunsjEventDto(
+                    eksternId = UUID.randomUUID(),
+                    journalpostId = JournalpostId("12345678"),
+                    eventTid = LocalDateTime.now(),
+                    status = Oppgavestatus.AAPEN,
+                    aktørId = AktørId("123"),
+                    aksjonspunktKoderMedStatusListe = mutableMapOf(),
+                    pleietrengendeAktørId = null,
+                    type = "PAPIRSØKNAD",
+                    ytelse = null,
+                    sendtInn = null,
+                    ferdigstiltAv = null,
+                    journalførtTidspunkt = null
+                ))
+            }
+        }
+    }
 }
+
