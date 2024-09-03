874a25efe9b4ab2cf3fe27e45f03486de5985a7c
vebnor <vebjorn.nordby@nav.no>
2024-09-03 12:15:03 +0200

    endre beskrivelse

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/kodeverk/BeskyttelseType.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/kodeverk/BeskyttelseType.kt
@@ -3,8 +3,8 @@ package no.nav.k9.los.nyoppgavestyring.kodeverk
 import com.fasterxml.jackson.annotation.JsonCreator
 
 enum class BeskyttelseType(val kode: String, val beskrivelse: String) {
-    KODE7("KODE7", "Kode 7"),
-    ORDINÆR("ORDINÆR", "Ingen beskyttelse");
+    KODE7("KODE7", "Fortrolig adresse"),
+    ORDINÆR("ORDINÆR", "Ugradert adresse");
 
     companion object {
         @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
