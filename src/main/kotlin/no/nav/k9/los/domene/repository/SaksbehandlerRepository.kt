1c48c9c0c298a371e228706271377965df655bc3
vebnor <vebjorn.nordby@nav.no>
2024-09-04 17:08:17 +0200

    skjuler disse kriteriene under avanserte valg for frontend

--- src/main/kotlin/no/nav/k9/los/domene/repository/SaksbehandlerRepository.kt
+++ src/main/kotlin/no/nav/k9/los/domene/repository/SaksbehandlerRepository.kt
@@ -360,6 +360,7 @@ class SaksbehandlerRepository(
     suspend fun hentAlleSaksbehandlere(): List<Saksbehandler> {
         Databasekall.map.computeIfAbsent(object {}.javaClass.name + object {}.javaClass.enclosingMethod.name) { LongAdder() }
             .increment()
+        pepClient.kanLeggeUtDriftsmelding()
         val skjermet = pepClient.harTilgangTilKode6()
         val identer = using(sessionOf(dataSource)) {
             it.run(
