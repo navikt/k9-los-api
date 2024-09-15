2df60fa20d0982b7f4f5dd99685c8a13bd1f10fc
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-09-15 12:18:20 +0200

    Forbedre test

--- src/test/kotlin/no/nav/k9/los/aksjonspunktbehandling/K9TilbakeEventHandlerTest.kt
+++ src/test/kotlin/no/nav/k9/los/aksjonspunktbehandling/K9TilbakeEventHandlerTest.kt
@@ -43,44 +43,23 @@ class K9TilbakeEventHandlerTest : AbstractK9LosIntegrationTest() {
 
     @Test
     fun `Skal ikke lage aktiv oppgave når behandlingen har aktivt autopunkt`() {
-
-        @Language("JSON") val json =
-            """{
-               "eksternId": "5c7be441-ebf3-4878-9ebc-399635b0a179",
-               "fagsystem": "K9TILBAKE",
-               "saksnummer": "61613602",
-               "aktørId": "9914079721604",
-               "behandlingId": null,
-               "eventTid": "2020-06-17T13:07:15.674343500",
-               "eventHendelse": "AKSJONSPUNKT_OPPRETTET",
-               "behandlinStatus": null,
-               "behandlingStatus": "UTRED",
-               "behandlingSteg": "FAKTFEILUTSTEG",
-               "behandlendeEnhet": "4833",
-               "ytelseTypeKode": "OMP",
-               "behandlingTypeKode": "BT-007",
-               "opprettetBehandling": "2020-06-16T13:16:51.690",
-               "aksjonspunktKoderMedStatusListe": {
-                               "5030": "UTFO",
-                               "7002": "UTFO",
-                               "7001": "OPPR",
-                               "7003": "OPPR"
-               },
-               "href": "/fpsak/fagsak/61613602/behandling/53/?punkt=default&fakta=default",
-               "førsteFeilutbetaling": "2019-10-19",
-               "feilutbetaltBeløp": 26820,
-               "ansvarligSaksbehandlerIdent": "saksbeh"
-}       """
+        val json = lagK9TilbakeEvent(FagsakYtelseType.OMSORGSPENGER,
+            mapOf(
+                "5030" to AksjonspunktStatus.UTFØRT,
+                "7002" to AksjonspunktStatus.UTFØRT,
+                "7001" to AksjonspunktStatus.OPPRETTET,
+                "7003" to AksjonspunktStatus.OPPRETTET
+            ))
 
         val event = AksjonspunktLagetTilbake().deserialize(null, json.toByteArray())!!
 
         k9TilbakeEventHandler.prosesser(event)
-        val oppgave = oppgaveRepository.hent(UUID.fromString("5c7be441-ebf3-4878-9ebc-399635b0a179"))
+        val oppgave = oppgaveRepository.hent(UUID.fromString("29cbdc33-0e59-4559-96a8-c2154bf17e5a"))
 
         assertTrue { !oppgave.aktiv } //oppgaven er ikke aktiv, siden behandlngen har autopunkt 7001
         val oppgaveV3 = transactionalManager.transaction { tx ->
             aktivOppgaveRepository.hentOppgaveForEksternId(tx,EksternOppgaveId("K9", "29cbdc33-0e59-4559-96a8-c2154bf17e5a"))}
-        assertThat(oppgaveV3).isNull()
+        assertThat(oppgaveV3!!.status).isEqualTo(Oppgavestatus.VENTER.kode)
     }
 
     @Test
