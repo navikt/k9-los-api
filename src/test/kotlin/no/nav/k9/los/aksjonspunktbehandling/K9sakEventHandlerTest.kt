2a60584c0093111a7cd819309d14cc311134a71d
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-09-15 13:05:56 +0200

    Fjernet duplisert test

--- src/test/kotlin/no/nav/k9/los/aksjonspunktbehandling/K9sakEventHandlerTest.kt
+++ src/test/kotlin/no/nav/k9/los/aksjonspunktbehandling/K9sakEventHandlerTest.kt
@@ -329,48 +329,6 @@ class K9sakEventHandlerTest : AbstractK9LosIntegrationTest() {
     }
 
     @Test
-    fun `Støtte tilbakekreving`() {
-        val k9TilbakeEventHandler = get<K9TilbakeEventHandler>()
-        val oppgaveRepository = get<OppgaveRepository>()
-
-
-        @Language("JSON") val json =
-            """{
-               "eksternId": "5c7be441-ebf3-4878-9ebc-399635b0a179",
-               "fagsystem": "K9TILBAKE",
-               "saksnummer": "61613602",
-               "aktørId": "9914079721604",
-               "behandlingId": null,
-               "eventTid": "2020-06-17T13:07:15.674343500",
-               "eventHendelse": "AKSJONSPUNKT_OPPRETTET",
-               "behandlinStatus": null,
-               "behandlingStatus": "UTRED",
-               "behandlingSteg": "FAKTFEILUTSTEG",
-               "behandlendeEnhet": "4833",
-               "ytelseTypeKode": "OMP",
-               "behandlingTypeKode": "BT-007",
-               "opprettetBehandling": "2020-06-16T13:16:51.690",
-               "aksjonspunktKoderMedStatusListe": {
-                               "5030": "UTFO",
-                               "7002": "UTFO",
-                               "7001": "OPPR",
-                               "7003": "OPPR"
-               },
-               "href": "/fpsak/fagsak/61613602/behandling/53/?punkt=default&fakta=default",
-               "førsteFeilutbetaling": "2019-10-19",
-               "feilutbetaltBeløp": 26820,
-               "ansvarligSaksbehandlerIdent": "saksbeh"
-}       """
-
-        val event = AksjonspunktLagetTilbake().deserialize(null, json.toByteArray())!!
-
-        k9TilbakeEventHandler.prosesser(event)
-        val oppgave =
-            oppgaveRepository.hent(UUID.fromString("5c7be441-ebf3-4878-9ebc-399635b0a179"))
-        assertTrue { !oppgave.aktiv }
-    }
-
-    @Test
     fun `Oppgave skal ende opp i kø`() {
         val k9sakEventHandler = get<K9sakEventHandler>()
         val oppgaveRepository = get<OppgaveRepository>()
