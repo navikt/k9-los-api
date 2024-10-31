6b36ac31ad0eedae00010cd7d178fea685c012b7
Mats Bolstad <mbolstad@gmail.com>
2024-10-31 14:26:28 +0100

    Manglet request context, som gjorde at pepclient ikke kunne finne idtoken

--- src/main/kotlin/no/nav/k9/los/tjenester/driftsmeldinger/DriftsmeldingerApis.kt
+++ src/main/kotlin/no/nav/k9/los/tjenester/driftsmeldinger/DriftsmeldingerApis.kt
@@ -6,46 +6,56 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import no.nav.k9.los.integrasjon.abac.IPepClient
+import no.nav.k9.los.integrasjon.rest.RequestContextService
 import no.nav.k9.los.tjenester.avdelingsleder.oppgaveko.IdDto
 import org.koin.ktor.ext.inject
 import java.util.*
 
 fun Route.DriftsmeldingerApis() {
-    val driftsmeldingTjeneste by inject<DriftsmeldingTjeneste>()
+    val requestContextService by inject<RequestContextService>()
     val pepClient by inject<IPepClient>()
+    val driftsmeldingTjeneste by inject<DriftsmeldingTjeneste>()
 
-    get("/") {
-        if (pepClient.harBasisTilgang()) {
-            call.respond(driftsmeldingTjeneste.hentDriftsmeldinger())
-        } else {
-            call.respond(HttpStatusCode.Forbidden)
+    get {
+        requestContextService.withRequestContext(call) {
+            if (pepClient.harBasisTilgang()) {
+                call.respond(driftsmeldingTjeneste.hentDriftsmeldinger())
+            } else {
+                call.respond(HttpStatusCode.Forbidden)
+            }
         }
     }
 
-    post("/") {
-        if (pepClient.harBasisTilgang()) {
-            val melding = call.receive<Driftsmelding>()
-            call.respond(driftsmeldingTjeneste.leggTilDriftsmelding(melding.driftsmelding))
-        } else {
-            call.respond(HttpStatusCode.Forbidden)
+    post {
+        requestContextService.withRequestContext(call) {
+            if (pepClient.kanLeggeUtDriftsmelding()) {
+                val melding = call.receive<Driftsmelding>()
+                call.respond(driftsmeldingTjeneste.leggTilDriftsmelding(melding.driftsmelding))
+            } else {
+                call.respond(HttpStatusCode.Forbidden)
+            }
         }
     }
 
     post("/slett") {
-        if (pepClient.harBasisTilgang()) {
-            val param = call.receive<IdDto>()
-            call.respond(driftsmeldingTjeneste.slettDriftsmelding(UUID.fromString(param.id)))
-        } else {
-            call.respond(HttpStatusCode.Forbidden)
+        requestContextService.withRequestContext(call) {
+            if (pepClient.kanLeggeUtDriftsmelding()) {
+                val param = call.receive<IdDto>()
+                call.respond(driftsmeldingTjeneste.slettDriftsmelding(UUID.fromString(param.id)))
+            } else {
+                call.respond(HttpStatusCode.Forbidden)
+            }
         }
     }
 
     post("/toggle") {
-        if (pepClient.harBasisTilgang()) {
-            val param = call.receive<DriftsmeldingSwitch>()
-            call.respond(driftsmeldingTjeneste.toggleDriftsmelding(param))
-        } else {
-            call.respond(HttpStatusCode.Forbidden)
+        requestContextService.withRequestContext(call) {
+            if (pepClient.kanLeggeUtDriftsmelding()) {
+                val param = call.receive<DriftsmeldingSwitch>()
+                call.respond(driftsmeldingTjeneste.toggleDriftsmelding(param))
+            } else {
+                call.respond(HttpStatusCode.Forbidden)
+            }
         }
     }
 }
