94d6e5fa64d615e9b5e01e84da347097c463dff3
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-12-16 20:12:40 +0100

    Konsistent kotlin-stlib, holder tilbake jackson for nå

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val kotliqueryVersion = "1.9.0"
 val k9SakVersion = "5.2.1"
 val k9KlageVersion = "0.4.5"
 val fuelVersion = "2.3.1"
-val jacksonVersion = "2.18.2"
+val jacksonVersion = "2.17.2"
 val commonsTextVersion = "1.13.0"
 
 val dusseldorfKtorVersion = "5.0.15"
@@ -26,7 +26,7 @@ val assertkVersion = "0.28.1"
 val mockkVersion = "1.13.13"
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.kotlin.jvm") version "2.0.21" //kan ikke gå for 2.1+ pga ktor som har avhengighet til kotlin-stdlib-common, og den er tilsynelatende ikke tilgjengelig i 2.1+
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
