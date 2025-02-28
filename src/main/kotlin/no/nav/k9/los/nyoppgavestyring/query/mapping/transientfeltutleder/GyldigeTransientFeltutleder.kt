658e441f7abc797f2c35dcb3dd4a081f8cede1fa
Boerta <bjorn.erik.lomo@nav.no>
2025-02-28 07:36:27 +0100

    Fjerne transientfeltutleder for beslutter nå som feltet er blitt persistent

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/query/mapping/transientfeltutleder/GyldigeTransientFeltutleder.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/query/mapping/transientfeltutleder/GyldigeTransientFeltutleder.kt
@@ -9,7 +9,6 @@ class GyldigeTransientFeltutleder {
 
     companion object {
         val feltutledere: Map<String, TransientFeltutleder> = hashMapOf(
-            K9BeslutterTransientFeltutleder::class.java.canonicalName to K9BeslutterTransientFeltutleder(),
             K9SakOppgavesaksbehandlingstidUtleder::class.java.canonicalName to K9SakOppgavesaksbehandlingstidUtleder(),
             K9SakVentetPåArbeidsgiverUtleder::class.java.canonicalName to K9SakVentetPåArbeidsgiverUtleder(),
             K9SakVentetPåSaksbehandlerUtleder::class.java.canonicalName to K9SakVentetPåSaksbehandlerUtleder(),
