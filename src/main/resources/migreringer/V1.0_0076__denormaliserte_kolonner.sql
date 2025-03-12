c91217e8e507ac00db45649ab578c5ef710e6b4c
Mats Bolstad <mbolstad@gmail.com>
2025-03-12 14:59:09 +0100

    Revert "Oppgavefeltverdi denormalisert. Migrer kun nye kolonner denne gangen."
    This reverts commit 8a0280211db74d08561479f294319dbcfa7a9ace.

--- src/main/resources/migreringer/V1.0_0076__denormaliserte_kolonner.sql
+++
@@ -1,3 +0,0 @@
-alter table oppgavefelt_verdi add column omrade_ekstern_id varchar(100);
-alter table oppgavefelt_verdi add column oppgavetype_ekstern_id varchar(100);
-alter table oppgavefelt_verdi add column feltdefinisjon_ekstern_id varchar(100);
