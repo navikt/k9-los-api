31d775a4001f1cddc63f4775c12c74d20c574772
Bjørn Erik Lømo <Bjorn.erik.lomo@nav.no>
2025-03-27 11:38:42 +0100

    Mappe nytt felt til dvh, forvaltningAPI for å resette dvh-sending (#2978)

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9/statistikk/OppgavestatistikkTjeneste.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9/statistikk/OppgavestatistikkTjeneste.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.runBlocking
 import kotliquery.TransactionalSession
 import no.nav.k9.los.Configuration
 import no.nav.k9.los.domene.lager.oppgave.v2.TransactionalManager
-import no.nav.k9.los.domene.modell.Fagsystem
 import no.nav.k9.los.integrasjon.abac.IPepClient
 import no.nav.k9.los.nyoppgavestyring.mottak.oppgavetype.OppgavetypeRepository
 import org.slf4j.LoggerFactory
@@ -141,7 +140,7 @@ class OppgavestatistikkTjeneste(
         }
     }
 
-    fun slettStatistikkgrunnlag(fagsystem: Fagsystem?) {
-        statistikkRepository.fjernSendtMarkering(fagsystem)
+    fun slettStatistikkgrunnlag(oppgavetype: String) {
+        statistikkRepository.fjernSendtMarkering(oppgavetype)
     }
 }
