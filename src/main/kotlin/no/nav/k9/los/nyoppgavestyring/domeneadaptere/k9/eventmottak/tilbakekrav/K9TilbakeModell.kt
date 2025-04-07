a4dc16b83a93ba4d7232016617517e48dd7888a2
Boerta <bjorn.erik.lomo@nav.no>
2025-04-07 15:26:45 +0200

    Ryddet K9TilbakeModell

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9/eventmottak/tilbakekrav/K9TilbakeModell.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9/eventmottak/tilbakekrav/K9TilbakeModell.kt
@@ -1,16 +1,17 @@
 package no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.eventmottak.tilbakekrav
 
 import no.nav.k9.los.domene.lager.oppgave.Oppgave
-import no.nav.k9.los.domene.modell.*
+import no.nav.k9.los.domene.modell.AksjonspunktTilstand
+import no.nav.k9.los.domene.modell.Aksjonspunkter
+import no.nav.k9.los.domene.modell.EventResultat
 import no.nav.k9.los.domene.repository.ReservasjonRepository
-import no.nav.k9.los.nyoppgavestyring.saksbehandleradmin.SaksbehandlerRepository
 import no.nav.k9.los.integrasjon.sakogbehandling.kontrakt.BehandlingAvsluttet
 import no.nav.k9.los.integrasjon.sakogbehandling.kontrakt.BehandlingOpprettet
-import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.eventmottak.IModell
 import no.nav.k9.los.nyoppgavestyring.kodeverk.AksjonspunktStatus
 import no.nav.k9.los.nyoppgavestyring.kodeverk.BehandlingStatus
 import no.nav.k9.los.nyoppgavestyring.kodeverk.BehandlingType
 import no.nav.k9.los.nyoppgavestyring.kodeverk.FagsakYtelseType
+import no.nav.k9.los.nyoppgavestyring.saksbehandleradmin.SaksbehandlerRepository
 import no.nav.k9.statistikk.kontrakter.Aktør
 import no.nav.k9.statistikk.kontrakter.Behandling
 import no.nav.k9.statistikk.kontrakter.Sak
@@ -22,10 +23,10 @@ import kotlin.math.min
 
 data class K9TilbakeModell(
     val eventer: List<K9TilbakeEventDto>
-) : IModell {
+) {
     private val `Omsorgspenger, Pleiepenger og opplæringspenger` = "ab0271"
 
-    override fun oppgave(): Oppgave {
+    fun oppgave(): Oppgave {
         return oppgave(sisteEvent())
     }
 
@@ -87,7 +88,7 @@ data class K9TilbakeModell(
         )
     }
 
-    override fun behandlingOpprettetSakOgBehandling(): BehandlingOpprettet {
+    fun behandlingOpprettetSakOgBehandling(): BehandlingOpprettet {
         val sisteEvent = sisteEvent()
         return BehandlingOpprettet(
             hendelseType = "behandlingOpprettet",
@@ -119,7 +120,7 @@ data class K9TilbakeModell(
         )
     }
 
-    override fun behandlingAvsluttetSakOgBehandling(
+    fun behandlingAvsluttetSakOgBehandling(
     ): BehandlingAvsluttet {
         val sisteEvent = sisteEvent()
         return BehandlingAvsluttet(
@@ -157,150 +158,8 @@ data class K9TilbakeModell(
         return this.eventer[this.eventer.lastIndex]
     }
 
-    fun forrigeEvent(): K9TilbakeEventDto? {
-        return if (this.eventer.lastIndex > 0) {
-            this.eventer[this.eventer.lastIndex - 1]
-        } else {
-            null
-        }
-    }
-
-    fun førsteEvent(): K9TilbakeEventDto {
-        return this.eventer[0]
-    }
-
-    override fun starterSak(): Boolean {
+    fun starterSak(): Boolean {
         return this.eventer.size == 1
-    }
-
-    override fun erTom(): Boolean {
-        return this.eventer.isEmpty()
-    }
-
-    override fun fikkEndretAksjonspunkt(): Boolean {
-        val forrigeEvent = forrigeEvent() ?: return false
-
-        // har blitt beslutter
-        if (!forrigeEvent.aktiveAksjonspunkt().tilBeslutterTilbake() &&
-            sisteEvent().aktiveAksjonspunkt().tilBeslutterTilbake()) {
-            return true
-        }
-        // har blitt satt på vent
-        if (sisteEvent().aktiveAksjonspunkt().påVentTilbake()) {
-            return true
-        }
-
-        // beslutter har gjort seg ferdig
-        if (forrigeEvent.aktiveAksjonspunkt().tilBeslutterTilbake() &&
-            sisteEvent().InaktiveAksjonspunkt().tilBeslutterTilbake())
-         {
-            return true
-        }
-
-        //beslutter sendt tilbake til saksbehandler
-        if(forrigeEvent.aktiveAksjonspunkt().tilBeslutterTilbake() &&
-            !sisteEvent().aktiveAksjonspunkt().tilBeslutterTilbake()) {
-            return true
-        }
-        // skal fortsette og ligge reservert
-        return false
-    }
-
-    // Array med alle versjoner av modell basert på eventene, brukes når man skal spille av eventer
-    fun alleVersjoner(): MutableList<K9TilbakeModell> {
-        val eventListe = mutableListOf<K9TilbakeEventDto>()
-        val modeller = mutableListOf<K9TilbakeModell>()
-        for (behandlingProsessEventDto in eventer) {
-            eventListe.add(behandlingProsessEventDto)
-            modeller.add(K9TilbakeModell(eventListe.toMutableList()))
-        }
-        return modeller
-    }
-
-    override fun dvhSak(): Sak {
-        val oppgave = oppgave(sisteEvent = sisteEvent())
-        val zone = ZoneId.of("Europe/Oslo")
-        return Sak(
-            saksnummer = oppgave.fagsakSaksnummer,
-            sakId = oppgave.fagsakSaksnummer,
-            funksjonellTid = sisteEvent().eventTid.atOffset(zone.rules.getOffset(sisteEvent().eventTid)),
-            tekniskTid = OffsetDateTime.now(),
-            opprettetDato = oppgave.behandlingOpprettet.toLocalDate(),
-            aktorId = oppgave.aktorId.toLong(),
-            aktorer = listOf(Aktør(oppgave.aktorId.toLong(), "Søker", "Søker")),
-            ytelseType = oppgave.fagsakYtelseType.navn,
-            underType = null,
-            sakStatus = oppgave.behandlingStatus.navn,
-            ytelseTypeBeskrivelse = null,
-            underTypeBeskrivelse = null,
-            sakStatusBeskrivelse = null,
-            avsender = "K9los",
-            versjon = 1
-        )
-    }
-
-    override fun sisteSaksNummer(): String {
-        return sisteEvent().saksnummer
-    }
-
-    override fun dvhBehandling(
-        saksbehandlerRepository: SaksbehandlerRepository,
-        reservasjonRepository: ReservasjonRepository
-    ): Behandling {
-        val oppgave = oppgave(sisteEvent())
-        val reservasjon = reservasjonRepository.hentOptional(oppgave.eksternId)
-        val beslutter = if (oppgave.tilBeslutter && reservasjon != null) {
-            val saksbehandler = saksbehandlerRepository.finnSaksbehandlerMedIdentInkluderKode6(reservasjon.reservertAv)
-            saksbehandler?.brukerIdent
-        } else {
-            ""
-        }
-
-        val behandldendeEnhet =
-            if (reservasjonRepository.finnes(oppgave.eksternId)) {
-                val hentMedHistorikk = reservasjonRepository.hentMedHistorikk(oppgave.eksternId)
-                val reservertav = hentMedHistorikk.map { it.reservertAv }.first()
-                saksbehandlerRepository.finnSaksbehandlerMedIdentInkluderKode6(reservertav)?.enhet?.substringBefore(" ")
-            } else {
-                "SRV"
-            }
-        val zone = ZoneId.of("Europe/Oslo")
-        return Behandling(
-            sakId = oppgave.fagsakSaksnummer,
-            behandlingId = oppgave.eksternId.toString(),
-            funksjonellTid = sisteEvent().eventTid.atOffset(zone.rules.getOffset(sisteEvent().eventTid)),
-            tekniskTid = OffsetDateTime.now(),
-            mottattDato = oppgave.behandlingOpprettet.toLocalDate(),
-            registrertDato = oppgave.behandlingOpprettet.toLocalDate(),
-            vedtaksDato = null,
-            relatertBehandlingId = null,
-            vedtakId = null,
-            saksnummer = oppgave.fagsakSaksnummer,
-            behandlingType = oppgave.behandlingType.navn,
-            behandlingStatus = oppgave.behandlingStatus.navn,
-            resultat = sisteEvent().resultatType,
-            resultatBegrunnelse = null,
-            utenlandstilsnitt = oppgave.utenlands.toString(),
-            behandlingTypeBeskrivelse = null,
-            behandlingStatusBeskrivelse = null,
-            resultatBeskrivelse = null,
-            resultatBegrunnelseBeskrivelse = null,
-            utenlandstilsnittBeskrivelse = null,
-            beslutter = beslutter,
-            saksbehandler = null,
-            behandlingOpprettetAv = "system",
-            behandlingOpprettetType = null,
-            behandlingOpprettetTypeBeskrivelse = null,
-            ansvarligEnhetKode = behandldendeEnhet,
-            ansvarligEnhetType = "NORG",
-            behandlendeEnhetKode = behandldendeEnhet,
-            behandlendeEnhetType = "NORG",
-            datoForUttak = null,
-            datoForUtbetaling = null,
-            totrinnsbehandling = oppgave.tilBeslutter,
-            avsender = "K9los",
-            versjon = 1
-        )
     }
 }
 
@@ -308,15 +167,7 @@ fun K9TilbakeEventDto.aktiveAksjonspunkt(): AksjonspunkterTilbake {
     return AksjonspunkterTilbake(this.aksjonspunktKoderMedStatusListe.filter { entry -> entry.value == "OPPR" })
 }
 
-fun K9TilbakeEventDto.InaktiveAksjonspunkt(): AksjonspunkterTilbake {
-    return AksjonspunkterTilbake(this.aksjonspunktKoderMedStatusListe.filter { entry -> entry.value != "OPPR" })
-}
-
 data class AksjonspunkterTilbake(val liste: Map<String, String>) {
-    fun lengde(): Int {
-        return liste.size
-    }
-
     fun erTom(): Boolean {
         return this.liste.isEmpty()
     }
