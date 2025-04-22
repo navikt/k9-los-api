c2cdb5874cbf674cc4c15665caa6065b934768ef
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-04-22 15:20:58 +0200

    Fjernet unødvendig annotasjon

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/infrastruktur/azuregraph/UserIdFilterResult.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/infrastruktur/azuregraph/UserIdFilterResult.kt
@@ -1,17 +1,14 @@
 package no.nav.k9.los.nyoppgavestyring.infrastruktur.azuregraph
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class UserId(
-    @JsonProperty("id")
     val id: UUID
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class UserIdFilterResult(
-
     val value: List<UserId>
 )
