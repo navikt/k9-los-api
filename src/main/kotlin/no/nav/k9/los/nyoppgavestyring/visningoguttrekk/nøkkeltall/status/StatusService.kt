ce83ead72d69047687eec7ea35681174c61373d2
Mats Bolstad <mbolstad@gmail.com>
2025-05-08 14:02:11 +0200

    Fiks opptelling avdelingslederstatus, grupperte ikke punsj riktig

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/visningoguttrekk/nøkkeltall/status/StatusService.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/visningoguttrekk/nøkkeltall/status/StatusService.kt
@@ -12,47 +12,35 @@ class StatusService(
 ) {
     private val log: Logger = LoggerFactory.getLogger(StatusService::class.java)
 
+    private val punsjtyper = setOf(
+        BehandlingType.PAPIRSØKNAD,
+        BehandlingType.DIGITAL_SØKNAD,
+        BehandlingType.PAPIRETTERSENDELSE,
+        BehandlingType.PAPIRINNTEKTSOPPLYSNINGER,
+        BehandlingType.DIGITAL_ETTERSENDELSE,
+        BehandlingType.INNLOGGET_CHAT,
+        BehandlingType.SKRIV_TIL_OSS_SPØRMSÅL,
+        BehandlingType.SKRIV_TIL_OSS_SVAR,
+        BehandlingType.SAMTALEREFERAT,
+        BehandlingType.KOPI,
+        BehandlingType.INNTEKTSMELDING_UTGÅTT,
+        BehandlingType.UTEN_FNR_DNR,
+        BehandlingType.PUNSJOPPGAVE_IKKE_LENGER_NØDVENDIG,
+        BehandlingType.UKJENT,
+    ).map { it.kode }
+
     fun hentStatus(harTilgangTilKode6: Boolean): List<OppgaverGruppertRepository.BehandlingstypeAntallDto> {
-        val grupperte =
+        val alleGrupper =
             oppgaverGruppertRepository.hentAntallÅpneOppgaverPrOppgavetypeBehandlingstype(harTilgangTilKode6)
-        val (medbehandlingType, utenBehandlingType) = grupperte.partition { it.behandlingstype != null }
-        if (utenBehandlingType.isNotEmpty()) {
-            val antall = utenBehandlingType.sumOf { it.antall }
-            log.warn("Fant $antall oppgaver uten behandlingstype, de blir ikke med oversikt som viser antall")
-        }
 
+        val (punsjGrupper, andreGrupper) = alleGrupper.partition { it.behandlingstype in punsjtyper }
 
-        val totaltAntall = OppgaverGruppertRepository.BehandlingstypeAntallDto(
-            "Åpne behandlinger",
-            oppgaverGruppertRepository.hentTotaltAntallÅpneOppgaver(harTilgangTilKode6)
-        )
-
-        val punsjtyper = setOf(
-            BehandlingType.PAPIRSØKNAD,
-            BehandlingType.DIGITAL_SØKNAD,
-            BehandlingType.PAPIRETTERSENDELSE,
-            BehandlingType.PAPIRINNTEKTSOPPLYSNINGER,
-            BehandlingType.DIGITAL_ETTERSENDELSE,
-            BehandlingType.INNLOGGET_CHAT,
-            BehandlingType.SKRIV_TIL_OSS_SPØRMSÅL,
-            BehandlingType.SKRIV_TIL_OSS_SVAR,
-            BehandlingType.SAMTALEREFERAT,
-            BehandlingType.KOPI,
-            BehandlingType.INNTEKTSMELDING_UTGÅTT,
-            BehandlingType.UTEN_FNR_DNR,
-            BehandlingType.PUNSJOPPGAVE_IKKE_LENGER_NØDVENDIG,
-            BehandlingType.UKJENT,
-        )
-        var punsjSum = 0
-        val mutableList = medbehandlingType.toMutableList()
-        medbehandlingType.forEach { antallDto ->
-            if (punsjtyper.any { it.kode == antallDto.behandlingstype }) {
-                punsjSum += antallDto.antall
-                mutableList.removeIf { it.behandlingstype == antallDto.behandlingstype }
+        return buildList {
+            add(OppgaverGruppertRepository.BehandlingstypeAntallDto("Åpne behandlinger", alleGrupper.sumOf { it.antall }))
+            addAll(andreGrupper)
+            if (punsjGrupper.isNotEmpty()) {
+                add(OppgaverGruppertRepository.BehandlingstypeAntallDto("Punsj", punsjGrupper.sumOf { it.antall }))
             }
         }
-        mutableList.add(OppgaverGruppertRepository.BehandlingstypeAntallDto("Punsj", punsjSum))
-
-        return listOf(totaltAntall).plus(mutableList)
     }
 }
