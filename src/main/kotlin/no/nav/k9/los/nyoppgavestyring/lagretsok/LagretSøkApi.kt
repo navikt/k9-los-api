a55a36d5e996e467b59928c764ee0502a51eb663
Mats Bolstad <mbolstad@gmail.com>
2025-05-23 10:35:37 +0200

    Lagt til metode for å finne alle for saksbehandler

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/lagretsok/LagretSøkApi.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/lagretsok/LagretSøkApi.kt
@@ -4,17 +4,14 @@ import io.github.smiley4.ktorswaggerui.dsl.routing.delete
 import io.github.smiley4.ktorswaggerui.dsl.routing.post
 import io.github.smiley4.ktorswaggerui.dsl.routing.get
 import io.github.smiley4.ktorswaggerui.dsl.routing.put
-import io.ktor.client.utils.EmptyContent.status
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.micrometer.core.instrument.binder.http.HttpJakartaServletRequestTags.status
 import no.nav.k9.los.nyoppgavestyring.infrastruktur.abac.IPepClient
 import no.nav.k9.los.nyoppgavestyring.infrastruktur.rest.RequestContextService
 import no.nav.k9.los.nyoppgavestyring.infrastruktur.rest.idToken
-import no.nav.k9.los.nyoppgavestyring.søkeboks.SøkeboksOppgaveDto
 import org.koin.ktor.ext.inject
 
 fun Route.LagretSøkApi() {
@@ -36,7 +33,7 @@ fun Route.LagretSøkApi() {
         requestContextService.withRequestContext(call) {
             if (pepClient.harBasisTilgang()) {
                 val id = call.parameters["id"]!!.toLong()
-                val lagretSøk = lagretSøkRepository.hentLagretSøk(id)
+                val lagretSøk = lagretSøkRepository.hent(id)
                 if (lagretSøk != null) {
                     call.respond(lagretSøk)
                 } else {
@@ -59,7 +56,7 @@ fun Route.LagretSøkApi() {
         requestContextService.withRequestContext(call) {
             if (pepClient.harBasisTilgang()) {
                 val request = call.receive<OpprettLagretSøk>()
-                val lagretSøk = lagretSøkTjeneste.opprettLagretSøk(coroutineContext.idToken().getNavIdent(), request)
+                val lagretSøk = lagretSøkTjeneste.opprett(coroutineContext.idToken().getNavIdent(), request)
                 call.respond(HttpStatusCode.Created, lagretSøk)
             } else {
                 call.respond(HttpStatusCode.Forbidden)
@@ -78,7 +75,7 @@ fun Route.LagretSøkApi() {
         requestContextService.withRequestContext(call) {
             if (pepClient.harBasisTilgang()) {
                 val endreLagretSøk = call.receive<EndreLagretSøk>()
-                val lagretSøk = lagretSøkTjeneste.endreLagretSøk(coroutineContext.idToken().getNavIdent(), endreLagretSøk)
+                val lagretSøk = lagretSøkTjeneste.endre(coroutineContext.idToken().getNavIdent(), endreLagretSøk)
                 call.respond(HttpStatusCode.OK, lagretSøk)
             } else {
                 call.respond(HttpStatusCode.Forbidden)
@@ -99,7 +96,7 @@ fun Route.LagretSøkApi() {
         requestContextService.withRequestContext(call) {
             if (pepClient.harBasisTilgang()) {
                 val lagretSøkId = call.parameters["id"]!!.toLong()
-                lagretSøkTjeneste.slettLagretSøk(coroutineContext.idToken().getNavIdent(), lagretSøkId)
+                lagretSøkTjeneste.slett(coroutineContext.idToken().getNavIdent(), lagretSøkId)
                 call.respond(HttpStatusCode.OK)
             } else {
                 call.respond(HttpStatusCode.Forbidden)
