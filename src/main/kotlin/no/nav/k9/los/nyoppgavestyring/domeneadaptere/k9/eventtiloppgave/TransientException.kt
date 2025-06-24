5196fe6d9866421fcb16fb557f145c8357fc3f42
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-06-24 18:28:26 +0200

    java->kotlin

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9/eventtiloppgave/TransientException.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/domeneadaptere/k9/eventtiloppgave/TransientException.kt
@@ -1,7 +1,3 @@
-package no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.eventtiloppgave;
+package no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.eventtiloppgave
 
-public class TransientException extends RuntimeException {
-    public TransientException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
+class TransientException(message: String, cause: Throwable) : RuntimeException(message, cause)
