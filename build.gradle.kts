046cccbcd9acfc43ff69f1896fbc34e94006e406
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-06-26 16:46:47 +0200

    Oppdater avhengighet til abac-kontrakt. Fjerner ubrukte Action-typer

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation("no.nav.common:auth:$navTilgangskontroll")
     implementation("no.nav.common:rest:$navTilgangskontroll")
     implementation("com.google.code.gson:gson:2.11.0")
-    implementation("no.nav.sif.abac:kontrakt:1.4.0")
+    implementation("no.nav.sif.abac:kontrakt:1.6.0")
 
     // Kontrakter
     implementation("no.nav.k9.sak:kontrakt:$k9SakVersion")
