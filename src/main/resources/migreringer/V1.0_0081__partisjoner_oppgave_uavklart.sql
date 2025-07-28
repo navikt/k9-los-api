9e01ec32f1ec6482cadaa2392ec5cd978aba67cc
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2025-07-28 11:25:36 +0200

    db-patch for å rebygge åpen-partisjon

---
+++ src/main/resources/migreringer/V1.0_0081__partisjoner_oppgave_uavklart.sql
@@ -0,0 +1,11 @@
+DROP TABLE oppgave_v3_aapen_venter_part
+
+-- Deler videre etter oppgavestatus-
+CREATE TABLE oppgave_v3_aapen_venter_uavklart_part
+    PARTITION OF oppgave_v3_part
+    FOR VALUES IN ('AAPEN', 'VENTER');
+
+-- Legg til constraint for å sikre at ferdigstilt_dato er NULL for AAPEN/VENTER
+ALTER TABLE oppgave_v3_aapen_venter_uavklart_part
+    ADD CONSTRAINT ferdigstilt_dato_is_null
+        CHECK (ferdigstilt_dato IS NULL);
---
+++ src/main/resources/migreringer/V1.0_0082__partisjoner_ofv_uavklart.sql
@@ -0,0 +1,11 @@
+DROP TABLE oppgavefelt_verdi_aapen_venter_part
+
+-- For aktive oppgaver, deler videre etter oppgavestatus
+CREATE TABLE oppgavefelt_verdi_aapen_venter_uavklart_part
+    PARTITION OF oppgavefelt_verdi_part
+    FOR VALUES IN ('AAPEN', 'VENTER');
+
+-- Legg til constraint for å sikre at ferdigstilt_dato er NULL for AAPEN/VENTER
+ALTER TABLE oppgavefelt_verdi_aapen_venter_part
+    ADD CONSTRAINT ferdigstilt_dato_is_null
+        CHECK (ferdigstilt_dato IS NULL);
