397b14df81b600dfd13bf5146c33fbafd2d8ceba
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2025-07-28 15:50:50 +0200

    utbygd unittest for sletting av saksbehandler

--- src/test/kotlin/no/nav/k9/los/nyoppgavestyring/saksbehandleradmin/SaksbehandlerRepositoryTest.kt
+++ src/test/kotlin/no/nav/k9/los/nyoppgavestyring/saksbehandleradmin/SaksbehandlerRepositoryTest.kt
@@ -2,11 +2,15 @@ package no.nav.k9.los.nyoppgavestyring.saksbehandleradmin
 
 import kotlinx.coroutines.runBlocking
 import no.nav.k9.los.AbstractK9LosIntegrationTest
+import no.nav.k9.los.nyoppgavestyring.FeltType
+import no.nav.k9.los.nyoppgavestyring.OppgaveTestDataBuilder
 import no.nav.k9.los.nyoppgavestyring.infrastruktur.db.TransactionalManager
+import no.nav.k9.los.nyoppgavestyring.reservasjon.ReservasjonV3Tjeneste
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 import org.koin.test.get
+import java.time.LocalDateTime
 
 class SaksbehandlerRepositoryTest : AbstractK9LosIntegrationTest() {
     @Test
@@ -31,6 +35,16 @@ class SaksbehandlerRepositoryTest : AbstractK9LosIntegrationTest() {
         }
 
         assertThat(saksbehandler!!.brukerIdent, equalTo(ident))
+
+        OppgaveTestDataBuilder()
+            .medOppgaveFeltVerdi(FeltType.AKSJONSPUNKT, "5016")
+            .lagOgLagre()
+
+        val reservasjonV3Tjeneste = get<ReservasjonV3Tjeneste>()
+
+        val reservasjon = reservasjonV3Tjeneste.taReservasjon("test", saksbehandler.id!!, saksbehandler.id!!, "test", LocalDateTime.now(), LocalDateTime.now().plusDays(1))
+
+        reservasjonV3Tjeneste.forlengReservasjon("test", LocalDateTime.now().plusDays(2), saksbehandler.id!!, "test")
 
         val transactionalManager = get<TransactionalManager>()
         transactionalManager.transaction { tx ->
