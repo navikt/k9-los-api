072ff202556b74583d89b41aaf5d04bb822c5cbe
Mats Bolstad <mbolstad@gmail.com>
2025-08-28 15:37:40 +0200

    Opplæringspenger i nye nøkkeltall

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/visningoguttrekk/nøkkeltall/ferdigstilteperenhet/FerdigstiltePerEnhetGruppe.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/visningoguttrekk/nøkkeltall/ferdigstilteperenhet/FerdigstiltePerEnhetGruppe.kt
@@ -6,6 +6,7 @@ enum class FerdigstiltePerEnhetGruppe(val navn: String, val ytelser: List<Fagsak
     ALLE("Alle ytelser", null),
     OMSORGSPENGER("Omsorgspenger", listOf(FagsakYtelseType.OMSORGSPENGER)),
     OMSORGSDAGER("Omsorgsdager", listOf(FagsakYtelseType.OMSORGSDAGER, FagsakYtelseType.OMSORGSPENGER_KS, FagsakYtelseType.OMSORGSPENGER_MA, FagsakYtelseType.OMSORGSPENGER_AO)),
+    OPPLÆRINGSPENGER("Opplæringspenger", listOf(FagsakYtelseType.OLP)),
     PLEIEPENGER_SYKT_BARN("Pleiepenger sykt barn", listOf(FagsakYtelseType.PLEIEPENGER_SYKT_BARN)),
     PPN("Pleiepenger i livets sluttfase", listOf(FagsakYtelseType.PPN)),
     PUNSJ("Punsj", null);
