679914d0b1d236d50208db7028523520a9855201
Mats Bolstad <mbolstad@gmail.com>
2025-09-10 12:33:13 +0200

    Ubrukt kode

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/visningoguttrekk/nøkkeltall/dagenstall/DagensTallUndergruppe.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/visningoguttrekk/nøkkeltall/dagenstall/DagensTallUndergruppe.kt
@@ -1,7 +1,5 @@
 package no.nav.k9.los.nyoppgavestyring.visningoguttrekk.nøkkeltall.dagenstall
 
-import no.nav.k9.los.nyoppgavestyring.kodeverk.BehandlingType
-
 enum class DagensTallUndergruppe(val navn: String) {
     TOTALT("Totalt"),
     FØRSTEGANG("Førstegangsbehandlinger"),
@@ -9,24 +7,5 @@ enum class DagensTallUndergruppe(val navn: String) {
     FEILUTBETALING("Feilutbetalinger"),
     KLAGE("Klager"),
     UNNTAKSBEHANDLING("Unntaksbehandlinger"),
-    PUNSJ("Punsj-oppgaver"),
-    ;
-
-    companion object {
-        fun fraBehandlingType(behandlingType: BehandlingType): DagensTallUndergruppe {
-            return when (behandlingType) {
-                BehandlingType.FORSTEGANGSSOKNAD -> FØRSTEGANG
-                BehandlingType.KLAGE -> KLAGE
-                BehandlingType.REVURDERING -> REVURDERING
-                BehandlingType.TILBAKE -> FEILUTBETALING
-                BehandlingType.REVURDERING_TILBAKEKREVING -> FEILUTBETALING
-                BehandlingType.UNNTAKSBEHANDLING -> UNNTAKSBEHANDLING
-                else -> throw IllegalArgumentException("Støtter ikke behandlingstype=$behandlingType")
-            }
-        }
-
-        fun forPunsj(): DagensTallUndergruppe {
-            return PUNSJ
-        }
-    }
+    PUNSJ("Punsj-oppgaver");
 }
