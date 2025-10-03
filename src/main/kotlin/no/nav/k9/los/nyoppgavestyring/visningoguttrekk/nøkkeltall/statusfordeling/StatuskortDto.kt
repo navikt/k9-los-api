a7629d60f5c2d5ebf50259af7a277a0afa4e66aa
Mats Bolstad <mbolstad@gmail.com>
2025-10-03 09:33:20 +0200

    Synlighet, tar vekk grupper

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/visningoguttrekk/nøkkeltall/statusfordeling/StatuskortDto.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/visningoguttrekk/nøkkeltall/statusfordeling/StatuskortDto.kt
@@ -4,13 +4,13 @@ import no.nav.k9.los.nyoppgavestyring.query.dto.query.OppgaveQuery
 import no.nav.k9.los.nyoppgavestyring.visningoguttrekk.nøkkeltall.KodeOgNavn
 
 data class StatuskortDto(
-    private val tittel: KodeOgNavn,
-    private val topplinje: StatuslinjeDto,
-    private val linjer: List<StatuslinjeDto> //funksjonell sortering
+    val tittel: KodeOgNavn,
+    val topplinje: StatuslinjeDto,
+    val linjer: List<StatuslinjeDto> //funksjonell sortering
 )
 
 data class StatuslinjeDto(
-    private val visningsnavn: String,
-    private val verdi: Long,
-    private val kildespørring: OppgaveQuery,
+    val visningsnavn: String,
+    val verdi: Long,
+    val kildespørring: OppgaveQuery,
 )
