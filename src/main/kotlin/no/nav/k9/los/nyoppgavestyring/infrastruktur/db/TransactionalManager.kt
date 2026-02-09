6c8a4c7b69eba0f5e7939798226a5208e5025e28
Mats Bolstad <mbolstad@gmail.com>
2026-02-09 18:36:15 +0100

    Fjerner ubrukt funksjon

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/infrastruktur/db/TransactionalManager.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/infrastruktur/db/TransactionalManager.kt
@@ -1,8 +1,5 @@
 package no.nav.k9.los.nyoppgavestyring.infrastruktur.db
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import kotliquery.TransactionalSession
 import kotliquery.sessionOf
 import kotliquery.using
@@ -15,18 +12,6 @@ class TransactionalManager(
         return using(sessionOf(dataSource, returnGeneratedKey = true)) { session ->
             session.transaction {
                 operation(it)
-            }
-        }
-    }
-
-    suspend fun <A> transactionSuspend(queryTimeout: Int? = null, operation: suspend (TransactionalSession) -> A): A {
-        return withContext(Dispatchers.IO) {
-            using(sessionOf(dataSource, returnGeneratedKey = true, queryTimeout = queryTimeout)) { session ->
-                session.transaction {
-                    runBlocking {
-                        operation(it)
-                    }
-                }
             }
         }
     }
