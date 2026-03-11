c0ddca3ce2a2730dce364cf029309c022397a02c
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2026-03-11 10:27:57 +0100

    Codereview

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/saksbehandleradmin/SaksbehandlerAdminTjeneste.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/saksbehandleradmin/SaksbehandlerAdminTjeneste.kt
@@ -51,7 +51,7 @@ class SaksbehandlerAdminTjeneste(
         transactionalManager.transaction { tx ->
             // V3-modellen: Sletter køer saksbehandler er med i
             oppgaveKøV3Repository.hentKoerMedOppgittSaksbehandler(tx, saksbehandler.id!!, skjermet, true).forEach { kø ->
-                oppgaveKøV3Repository.endre(tx, kø.copy(saksbehandlere = kø.saksbehandlere - saksbehandler.epost), skjermet)
+                oppgaveKøV3Repository.endre(tx, kø.copy(saksbehandlerIds = kø.saksbehandlerIds - saksbehandler.id!!), skjermet)
             }
 
             // Sletter fra saksbehandler-tabellen
