4270fa80c704b809f26359e3d39bc1a80d3318ad
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2026-05-08 09:35:31 +0200

    Fjerne backfill, koble logikk mot ny kvitteringstabell

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/forvaltning/ForvaltningApis.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/forvaltning/ForvaltningApis.kt
@@ -7,8 +7,6 @@ import io.ktor.server.routing.*
 import kotliquery.queryOf
 import no.nav.k9.kodeverk.behandling.FagsakYtelseType
 import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.avstemming.AvstemmingsTjeneste
-import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.statistikk.StatistikkRepository
-import no.nav.k9.los.nyoppgavestyring.domeneadaptere.k9.eventmottak.eventlager.EventRepository
 import no.nav.k9.los.nyoppgavestyring.infrastruktur.abac.IPepClient
 import no.nav.k9.los.nyoppgavestyring.infrastruktur.db.TransactionalManager
 import no.nav.k9.los.nyoppgavestyring.infrastruktur.rest.RequestContextService
@@ -38,7 +36,6 @@ fun Route.forvaltningApis() {
     val transactionalManager by inject<TransactionalManager>()
     val forvaltningRepository by inject<ForvaltningRepository>()
     val avstemmingsTjeneste by inject<AvstemmingsTjeneste>()
-    val statistikkRepository by inject<StatistikkRepository>()
 
     val pepClient by inject<IPepClient>()
     val requestContextService by inject<RequestContextService>()
@@ -487,19 +484,6 @@ fun Route.forvaltningApis() {
         }
     }
 
-    get("/backfill-sendt-dvh-ekstern", {
-        tags("Forvaltning")
-        description = "Engangs-backfill: Populerer OPPGAVE_V3_SENDT_DVH_EKSTERN basert på eksisterende data i OPPGAVE_V3_SENDT_DVH. Kan trygt kjøres flere ganger."
-    }) {
-        requestContextService.withRequestContext(call) {
-            if (pepClient.kanLeggeUtDriftsmelding()) {
-                val antallBackfilled = statistikkRepository.backfillSendtDvhEkstern()
-                call.respond("Backfill ferdig. $antallBackfilled rader lagt til i OPPGAVE_V3_SENDT_DVH_EKSTERN.")
-            } else {
-                call.respond(HttpStatusCode.Forbidden)
-            }
-        }
-    }
 
 }
 
