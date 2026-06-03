0cbc009b2b82d1b9918da75fbf8a6e65ff35a906
Mats Bolstad <mbolstad@gmail.com>
2026-06-03 08:51:18 +0200

    fjerner unødvendig kommentar

--- src/main/resources/migreringer/V1.0_0105__oppgave_v3_part_reservasjonsnokkel_index.sql
+++ src/main/resources/migreringer/V1.0_0105__oppgave_v3_part_reservasjonsnokkel_index.sql
@@ -1,4 +1,2 @@
--- Index for effektivt oppslag av åpne/ventende/uavklarte oppgaver gitt reservasjonsnøkkel.
--- Legges direkte på leaf-partisjonen for AAPEN/VENTER/UAVKLART.
 CREATE INDEX idx_oppgave_v3_part_avu_reservasjonsnokkel
     ON oppgave_v3_aapen_venter_uavklart_part (reservasjonsnokkel);
