0fc411caf1e33e3bd060ac13f6df4b45d49ad912
Mats Bolstad <mbolstad@gmail.com>
2026-06-05 09:50:10 +0200

    Feil etter merge

--- src/main/kotlin/no/nav/k9/los/nyoppgavestyring/forvaltning/ForvaltningApis.kt
+++ src/main/kotlin/no/nav/k9/los/nyoppgavestyring/forvaltning/ForvaltningApis.kt
@@ -1,5 +1,7 @@
 package no.nav.k9.los.nyoppgavestyring.forvaltning
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
 import io.github.smiley4.ktoropenapi.get
 import io.github.smiley4.ktoropenapi.post
 import io.ktor.http.*
