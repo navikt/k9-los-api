0863ea83183107cc9d98a5dacc558655350f85d4
Mats Bolstad <mbolstad@gmail.com>
2026-09-02 18:37:33 +0200

    startup probe

--- src/main/resources/migreringer/V1.0_0108__omrade_kobling.sql
+++ src/main/resources/migreringer/V1.0_0108__omrade_kobling.sql
@@ -1,6 +1,7 @@
 -- Kobler område til tabellene som må avgrenses når flere områder tas i bruk.
--- Eksisterende rader tilhører K9. Validering og indeksbygging tas i egne migreringer
--- fordi de leser gjennom de store tabellene.
+-- Eksisterende rader tilhører K9. Constraints opprettes som NOT VALID slik at denne
+-- migreringen ikke skanner de store tabellene. Valideringen skjer etter commit i
+-- V1.0_0109, slik at låsene fra tabellendringene slippes før skanningen starter.
 
 insert into omrade(ekstern_id)
 values ('K9')
@@ -17,11 +18,9 @@ begin
     where ekstern_id = 'K9';
 
     execute format('alter table reservasjon_v3 add column omrade_id bigint not null default %s', k9_id);
-    alter table reservasjon_v3 alter column omrade_id drop default;
     alter table reservasjon_v3 add constraint fk_reservasjon_v3_omrade foreign key (omrade_id) references omrade (id) not valid;
 
     execute format('alter table oppgaveko_v3 add column omrade_id bigint not null default %s', k9_id);
-    alter table oppgaveko_v3 alter column omrade_id drop default;
     alter table oppgaveko_v3 add constraint fk_oppgaveko_v3_omrade foreign key (omrade_id) references omrade (id) not valid;
 
     create table if not exists saksbehandler_omrade
@@ -39,15 +38,12 @@ begin
             );
 
     execute format('alter table lagret_sok add column omrade_id bigint not null default %s', k9_id);
-    alter table lagret_sok alter column omrade_id drop default;
     alter table lagret_sok add constraint fk_lagret_sok_omrade foreign key (laget_av, omrade_id) references saksbehandler_omrade (saksbehandler_id, omrade_id) not valid;
 
     execute format('alter table oppgave_pep_cache add column omrade_id bigint not null default %s', k9_id);
-    alter table oppgave_pep_cache alter column omrade_id drop default;
     alter table oppgave_pep_cache add constraint fk_oppgave_pep_cache_omrade foreign key (omrade_id) references omrade (id) not valid;
 
     execute format('alter table event_nokkel add column omrade_id bigint not null default %s', k9_id);
-    alter table event_nokkel alter column omrade_id drop default;
     alter table event_nokkel add constraint fk_event_nokkel_omrade foreign key (omrade_id) references omrade (id) not valid;
 
     execute format('alter table oppgave_id_part add column omrade_id bigint not null default %s', k9_id);
@@ -55,7 +51,7 @@ begin
 end
 $$;
 
--- Små tabeller valideres med en gang. Store tabeller valideres i V1.0_0109.
+-- Små tabeller valideres med en gang. Store tabeller valideres etter commit i V1.0_0109.
 alter table saksbehandler_omrade validate constraint fk_saksbehandler_omrade_omrade;
 alter table oppgaveko_v3 validate constraint fk_oppgaveko_v3_omrade;
 
