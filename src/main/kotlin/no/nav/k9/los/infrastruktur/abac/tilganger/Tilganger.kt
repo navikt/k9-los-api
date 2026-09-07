dbd2ec9d37717b72cd6e0daae84d2c1c4e0f41ca
Mats Bolstad <mbolstad@gmail.com>
2026-09-07 18:17:01 +0200

    Kompileringsfeil

--- src/main/kotlin/no/nav/k9/los/infrastruktur/abac/tilganger/Tilganger.kt
+++ src/main/kotlin/no/nav/k9/los/infrastruktur/abac/tilganger/Tilganger.kt
@@ -1,6 +1,6 @@
 package no.nav.k9.los.infrastruktur.abac.tilganger
 
-internal data class Tilganger(
+data class Tilganger(
     val basis: Boolean,
     val kode6: Boolean,
     val oppgavestyring: Boolean,
