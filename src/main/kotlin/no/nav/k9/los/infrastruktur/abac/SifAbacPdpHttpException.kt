bac90401fffed75db4cf53f90cf39031b15eb115
Mats Bolstad <mbolstad@gmail.com>
2026-09-07 18:07:43 +0200

    Synlighet dataklasser, ihht. review

--- src/main/kotlin/no/nav/k9/los/infrastruktur/abac/SifAbacPdpHttpException.kt
+++ src/main/kotlin/no/nav/k9/los/infrastruktur/abac/SifAbacPdpHttpException.kt
@@ -5,7 +5,7 @@ package no.nav.k9.los.infrastruktur.abac
  * feilhåndteringen de øvrige kallene i [SifAbacPdpKlient] bruker, men eksponerer statuskoden typet
  * slik at kallere kan logge den uten å måtte parse feilmeldingen.
  */
-class SifAbacPdpHttpException(
+internal class SifAbacPdpHttpException(
     val status: Int,
     operasjon: String,
 ) : IllegalStateException("Feil ved '$operasjon' mot sif-abac-pdp: HTTP $status")
--- src/main/kotlin/no/nav/k9/los/infrastruktur/abac/tilganger/Tilganger.kt
+++ src/main/kotlin/no/nav/k9/los/infrastruktur/abac/tilganger/Tilganger.kt
@@ -1,6 +1,6 @@
 package no.nav.k9.los.infrastruktur.abac.tilganger
 
-data class Tilganger(
+internal data class Tilganger(
     val basis: Boolean,
     val kode6: Boolean,
     val oppgavestyring: Boolean,
