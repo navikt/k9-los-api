f9f31c66fcdedd79ef0f8b8fdca3119467bc7e6d
Mats Bolstad <mbolstad@gmail.com>
2026-09-14 17:04:42 +0200

    Analyze i partisjonerte tabeller

--- src/main/resources/migreringer/V1.0_0109__legg_til_omrade_ekstern_id_pa_oppgavepartisjoner.sql
+++ src/main/resources/migreringer/V1.0_0109__legg_til_omrade_ekstern_id_pa_oppgavepartisjoner.sql
@@ -3,3 +3,7 @@ alter table oppgave_v3_part
 
 alter table oppgavefelt_verdi_part
     add column omrade_ekstern_id varchar(100) not null default 'K9';
+
+analyze oppgave_v3_part;
+
+analyze oppgavefelt_verdi_part;
